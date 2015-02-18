f2800a26f793823eb9eb2e7cc8fbc7180d36ce37
Nicholas J Meyer <nick.j.meyer@gmail.com>
2015-02-18 12:38:21 -0500

    Edit number of treatments to 0.03
    Needs to be a reasonable value.  Before it was 0.026.

--- src/agent.cpp
+++ src/agent.cpp
@@ -4,7 +4,7 @@ int getNumPre(const SimData & sD,
 	      const TrtData & tD,
 	      const FixedData & fD,
 	      const DynamicData & dD){
-  return std::min((int)std::floor(0.026*((double)fD.numNodes)+1),
+  return std::min((int)std::floor(0.03*((double)fD.numNodes)+1),
   		  sD.numNotInfec);
   // return std::min(1,sD.numNotInfec);
 }
@@ -15,7 +15,7 @@ int getNumAct(const SimData & sD,
 	      const TrtData & tD,
 	      const FixedData & fD,
 	      const DynamicData & dD){
-  return std::min((int)std::floor(0.026*((double)fD.numNodes)+1),
+  return std::min((int)std::floor(0.03*((double)fD.numNodes)+1),
   		  sD.numInfected);
   // return std::min(1,sD.numInfected);
 }
