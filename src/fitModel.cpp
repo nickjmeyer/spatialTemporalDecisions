73b9d0246185043e2f4665ea99799f73aa18cc3a
Nick Meyer <nick@nick-laptop>
2015-03-14 08:56:18 -0400

    Get ready to fit generative models for simulations

--- src/fitModel.cpp
+++ src/fitModel.cpp
@@ -1,65 +1,57 @@
 #include "fitModel.hpp"
 
+
+template<class M, class P>
+void fitModel(const int numSamples,const int numBurn,const int verbose){
+  typedef System<M,P,M,P> S;
+  
+  S s("obsData.txt");
+  
+  s.paramEst_r = s.paramGen_r;
+  s.reset();
+
+  s.modelGen.fitType = MLE;
+  s.modelGen.fit(s.sD,s.tD,s.fD,s.dD,s.paramGen);
+
+  std::vector<double> mlePar = s.paramGen.getPar();
+
+  s.modelGen.mcmc.load(s.sD.history,s.sD.status,s.fD);
+  s.modelGen.mcmc.sample(20000,10000);
+  s.modelGen.mcmc.samples.setMean();
+  s.paramGen.putPar(s.modelGen.mcmc.samples.getPar());
+
+  s.paramGen.save();
+
+  std::vector<double> mcmcPar = s.paramGen.getPar();
+
+#pragma omp critical (verbose)
+  {
+    if(verbose){
+      njm::message("MCMC: " + njm::toString(mlePar," "));
+      njm::message("MCMC: " + njm::toString(mcmcPar," "));
+    }
+  }
+}
+
 int main(int argc, char ** argv){
   njm::sett.set(argc,argv);
 
+  int numSamples=20000,numBurn=5000;
+
+#pragma omp parallel sections			\
+  shared(numSamples,numBurn)
   {
-    njm::message("Gravity Model without time infected");
+#pragma omp section
+    {
+      njm::message("Gravity Model: no time infected");
     
-    typedef GravityModel GM;
-    typedef GravityParam GP;
-    typedef GravityModel EM;
-    typedef GravityParam EP;
+      typedef GravityModel M;
+      typedef GravityParam P;
 
-    typedef System<GM,GP,EM,EP> S;
-  
-    S s("obsData.txt");
-  
-    s.paramEst_r = s.paramGen_r;
-    s.reset();
-
-    s.modelGen.fitType = MLE;
-    s.modelGen.fit(s.sD,s.tD,s.fD,s.dD,s.paramGen);
-
-    njm::message(" MLE: " + njm::toString(s.paramGen.getPar()," "));
-
-    s.modelGen.fitType = MCMC;
-    s.modelGen.fit(s.sD,s.tD,s.fD,s.dD,s.paramGen);
-
-    njm::message("MCMC: " + njm::toString(s.paramGen.getPar()," "));
-
-    s.paramGen.save();
-  }
-
-
-  {
-    njm::message("Gravity Model with time infected");
+      fitModel<M,P>(numSamples,numBurn,1);
+    }
     
-    typedef GravityTimeInfModel GM;
-    typedef GravityTimeInfParam GP;
-    typedef GravityTimeInfModel EM;
-    typedef GravityTimeInfParam EP;
-
-    typedef System<GM,GP,EM,EP> S;
-  
-    S s("obsData.txt");
-  
-    s.paramEst_r = s.paramGen_r;
-    s.reset();
-
-    s.modelGen.fitType = MLE;
-    s.modelGen.fit(s.sD,s.tD,s.fD,s.dD,s.paramGen);
-
-    njm::message(" MLE: " + njm::toString(s.paramGen.getPar()," "));
-
-    s.modelGen.fitType = MCMC;
-    s.modelGen.fit(s.sD,s.tD,s.fD,s.dD,s.paramGen);
-
-    njm::message("MCMC: " + njm::toString(s.paramGen.getPar()," "));
-
-    s.paramGen.save();
   }
-
 
   njm::sett.clean();
   return 0;
