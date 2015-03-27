2a21fac32c66a114ee67988f65c5b2a8a39f0d2f
Nicholas J Meyer <nick.j.meyer@gmail.com>
2015-03-27 10:30:52 -0400

    Edit test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -11,16 +11,16 @@ int main(int argc, char ** argv){
 
   typedef System<GM,GP,EM,EP> S;
 
-  typedef ToyFeatures2<EM,EP> F;
-  typedef RankAgent<F,EM,EP> RA;
+  typedef NoTrt<EM,EP> NT;
 
-  typedef FeaturesInt<F,EM,EP> FI;
-  typedef M2QOptim<S,RA,FI,EM,EP> OQ;
+  typedef VanillaRunner<S,NT> VR;
 
   S s;
 
-  RA ra;
-  OQ oq;
+  int numReps = 500;
+
+  NT nt;
+  VR vr;
 
   s.modelGen.fitType = MLE;
   s.modelEst.fitType = MLE;
@@ -28,31 +28,59 @@ int main(int argc, char ** argv){
   s.paramEst_r = s.paramGen_r;
   s.reset();
 
-  int i;
-  for(i = 0; i < 1; i++)
-    njm::runif01();
+  omp_set_num_threads(4);
+
+  std::cout << vr.run(s,nt,numReps,s.fD.finalT) << std::endl;
   
-  int t;
-  for(t = 0; t < 10; ++t){
-    if(t >= s.fD.trtStart){
-      ra.applyTrt(s.sD,s.tD,s.fD,s.dD,s.modelGen,s.paramEst);
-    }
-    s.nextPoint();
+  // typedef GravityTimeInfExpCavesModel GM;
+  // typedef GravityTimeInfExpCavesParam GP;
+  // typedef GM EM;
+  // typedef GP EP;
 
-  }
+  // typedef System<GM,GP,EM,EP> S;
 
-  std::cout << "value: " << s.value() << std::endl;
+  // typedef ToyFeatures2<EM,EP> F;
+  // typedef RankAgent<F,EM,EP> RA;
+
+  // typedef FeaturesInt<F,EM,EP> FI;
+  // typedef M2QOptim<S,RA,FI,EM,EP> OQ;
+
+  // S s;
+
+  // RA ra;
+  // OQ oq;
+
+  // s.modelGen.fitType = MLE;
+  // s.modelEst.fitType = MLE;
+
+  // s.paramEst_r = s.paramGen_r;
+  // s.reset();
+
+  // int i;
+  // for(i = 0; i < 1; i++)
+  //   njm::runif01();
   
-  oq.qEval.preCompData(s.sD,s.fD);
+  // int t;
+  // for(t = 0; t < 10; ++t){
+  //   if(t >= s.fD.trtStart){
+  //     ra.applyTrt(s.sD,s.tD,s.fD,s.dD,s.modelGen,s.paramEst);
+  //   }
+  //   s.nextPoint();
 
-  oq.qEval.bellResFixData(s.sD,s.tD,s.fD,s.dD,s.modelEst,s.paramEst);
+  // }
 
-  oq.qEval.bellResPolData(s.sD.time,s.fD,s.modelEst,s.paramEst,ra);
+  // std::cout << "value: " << s.value() << std::endl;
+  
+  // oq.qEval.preCompData(s.sD,s.fD);
 
-  oq.qEval.buildRD();
+  // oq.qEval.bellResFixData(s.sD,s.tD,s.fD,s.dD,s.modelEst,s.paramEst);
 
-  for(i = 0; i < 10; ++i)
-    oq.qEval.solve();
+  // oq.qEval.bellResPolData(s.sD.time,s.fD,s.modelEst,s.paramEst,ra);
+
+  // oq.qEval.buildRD();
+
+  // for(i = 0; i < 10; ++i)
+  //   oq.qEval.solve();
 
 
   
