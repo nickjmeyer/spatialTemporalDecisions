cb63a128d3173424b6b0054d8c8cb1ee559c02d5
Nicholas J Meyer <nick.j.meyer@gmail.com>
2015-04-07 01:11:23 -0400

    Run no active for other networks

--- src/agent.cpp
+++ src/agent.cpp
@@ -5,10 +5,10 @@ int getNumPre(const SimData & sD,
 	      const TrtData & tD,
 	      const FixedData & fD,
 	      const DynamicData & dD){
-  // return std::min((int)std::floor(0.12*((double)fD.numNodes)+1),
-  // 		  sD.numNotInfec);
-  return std::min((int)std::floor(0.06*((double)fD.numNodes)+1),
+  return std::min((int)std::floor(0.12*((double)fD.numNodes)+1),
   		  sD.numNotInfec);
+  // return std::min((int)std::floor(0.06*((double)fD.numNodes)+1),
+  // 		  sD.numNotInfec);
   // return std::min(1,sD.numNotInfec);
 }
 
@@ -18,8 +18,8 @@ int getNumAct(const SimData & sD,
 	      const TrtData & tD,
 	      const FixedData & fD,
 	      const DynamicData & dD){
-  // return 0;
-  return std::min((int)std::floor(0.06*((double)fD.numNodes)+1),
-  		  sD.numInfected);
+  return 0;
+  // return std::min((int)std::floor(0.06*((double)fD.numNodes)+1),
+  // 		  sD.numInfected);
   // return std::min(1,sD.numInfected);
 }
