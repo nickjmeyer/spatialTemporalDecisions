217e2d9b74e2ab5833a27e5ddf29e5c73abf0c7e
Nicholas J Meyer <nick.j.meyer@gmail.com>
2015-04-07 15:45:23 -0400

    Test timing

--- src/test.cpp
+++ src/test.cpp
@@ -5,39 +5,81 @@ int main(int argc, char ** argv){
   njm::sett.set(argc,argv);
 
   typedef GravityTimeInfExpCavesModel MG;
-  
-  typedef MultiModel ME;
+  typedef GravityTimeInfExpCavesModel ME;
 
   typedef System<MG,ME> S;
+  
+  typedef ToyFeatures2<ME> F;
 
-  typedef ToyFeatures2Multi<ME> F;
-  typedef FeaturesInt<F,ME> FI;
+  typedef NoTrt<ME> NT;
   typedef RankAgent<F,ME> RA;
 
-  typedef M2QOptim<S,RA,FI,ME> SPO;
-
-  typedef OptimRunner<S,RA,SPO> R_RA;
-
+  typedef VanillaRunnerNS<S,NT> VNT;
+  typedef VanillaRunnerNS<S,RA> VRA;
 
   S s;
+
   s.modelGen.setType(MLE);
   s.modelEst.setType(MLE);
 
-  int numReps = 1;
+  int numReps = 10000;
   Starts starts(numReps,s.fD.numNodes);
  
+  NT nt;
   RA ra; // running at the good starting weights
 
   ra.tp.weights_r.zeros(ra.f.numFeatures);
   ra.tp.weights_r(2) = 1;
 
-  SPO spo;
+  ra.tp.weights_r.ones(ra.f.numFeatures);
 
-  R_RA r_ra;
+  VNT vnt;
+  VRA vra;
 
-  njm::message("Priority Score: "
-  	       + njm::toString(r_ra.run(s,ra,spo,numReps,s.fD.finalT,starts),
-  			       ""));
+
+  njm::timer.start("no trt");
+  vnt.run(s,nt,numReps,s.fD.finalT,starts);
+  njm::timer.stop("no trt");
+
+
+  njm::timer.start("rank");
+  vra.run(s,ra,numReps,s.fD.finalT,starts);
+  njm::timer.stop("rank");
+
+  
+
+  
+
+  // typedef MultiModel ME;
+
+  // typedef System<MG,ME> S;
+
+  // typedef ToyFeatures2Multi<ME> F;
+  // typedef FeaturesInt<F,ME> FI;
+  // typedef RankAgent<F,ME> RA;
+
+  // typedef M2QOptim<S,RA,FI,ME> SPO;
+
+  // typedef OptimRunner<S,RA,SPO> R_RA;
+
+
+  // S s;
+  // s.modelGen.setType(MLE);
+  // s.modelEst.setType(MLE);
+
+  // int numReps = 1;
+  // Starts starts(numReps,s.fD.numNodes);
+ 
+  // RA ra; // running at the good starting weights
+
+  // // ra.tp.weights_r.zeros(ra.f.numFeatures);
+  // // ra.tp.weights_r(2) = 1;
+
+  // ra.tp.weights_r.ones(ra.f.numFeatures);
+
+  // njm::message("Priority Score: "
+  // 	       + njm::toString(r_ra.run(s,ra,spo,numReps,s.fD.finalT,starts),
+  // 			       ""));
 
   
 
