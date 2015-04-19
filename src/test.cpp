ce8ef7dfad6a83505d79d74f3f34266f91f2cea8
Nicholas J Meyer <nick.j.meyer@gmail.com>
2015-04-18 20:26:17 -0400

    Timing

--- src/test.cpp
+++ src/test.cpp
@@ -42,7 +42,7 @@ int main(int argc, char ** argv){
   std::vector<double> par = s.modelGen_r.getPar();
   s.modelEst_r.putPar(par.begin());
 
-  int numReps = 500;
+  int numReps = 1000;
   Starts starts("startingLocations.txt");
 
   s.reset(starts[0]);
