eca7ae4814d0f41971dc6d91f1c9b13a5e690539
Nicholas J Meyer <nick.j.meyer@gmail.com>
2015-05-17 16:11:18 -0700

    debugging

--- src/runM1Mles.cpp
+++ src/runM1Mles.cpp
@@ -11,21 +11,21 @@ int main(int argc, char ** argv){
   typedef System<MG,ME> S;
 
   typedef NoTrt<ME> NT;
-  typedef ProximalGDistAgent<ME> PA;
-  typedef MyopicAgent<ME> MA;
+  // typedef ProximalGDistAgent<ME> PA;
+  // typedef MyopicAgent<ME> MA;
   
-  typedef ToyFeatures5<ME> F;
-  typedef RankAgent<F,ME> RA;
-  typedef OsspAgent<ME> OA;
+  // typedef ToyFeatures5<ME> F;
+  // typedef RankAgent<F,ME> RA;
+  // typedef OsspAgent<ME> OA;
 
-  typedef M1SpOptim<S,RA,ME> SPO;
-  typedef M1OsspOptim<S,OA,F,ME> OSSPO;
+  // typedef M1SpOptim<S,RA,ME> SPO;
+  // typedef M1OsspOptim<S,OA,F,ME> OSSPO;
 
   typedef VanillaRunner<S,NT> R_NT;
-  typedef VanillaRunner<S,PA> R_PA;
-  typedef FitOnlyRunner<S,MA> R_MA;
-  typedef OptimRunner<S,RA,SPO> R_RA;
-  typedef OptimRunner<S,OA,OSSPO> R_OA;
+  // typedef VanillaRunner<S,PA> R_PA;
+  // typedef FitOnlyRunner<S,MA> R_MA;
+  // typedef OptimRunner<S,RA,SPO> R_RA;
+  // typedef OptimRunner<S,OA,OSSPO> R_OA;
 
 
   S s;
@@ -36,19 +36,19 @@ int main(int argc, char ** argv){
   Starts starts(numReps,s.fD.numNodes);
 
   NT nt;
-  PA pa;
-  MA ma;
-  RA ra;
-  OA oa;
+  // PA pa;
+  // MA ma;
+  // RA ra;
+  // OA oa;
 
-  SPO spo;
-  OSSPO osspo;
+  // SPO spo;
+  // OSSPO osspo;
 
   R_NT r_nt;
-  R_PA r_pa;
-  R_MA r_ma;
-  R_RA r_ra;
-  R_OA r_oa;
+  // R_PA r_pa;
+  // R_MA r_ma;
+  // R_RA r_ra;
+  // R_OA r_oa;
 
 
   RunStats rs;
@@ -103,29 +103,29 @@ int main(int argc, char ** argv){
 	       + njm::toString(rs.smean(),"")
 	       + "  (" + njm::toString(rs.seMean(),"") + ")");
   
-  rs = r_pa.run(s,pa,numReps,s.fD.finalT,starts);
-  njm::message("       Proximal: "
-	       + njm::toString(rs.smean(),"")
-	       + "  (" + njm::toString(rs.seMean(),"") + ")");
+  // rs = r_pa.run(s,pa,numReps,s.fD.finalT,starts);
+  // njm::message("       Proximal: "
+  // 	       + njm::toString(rs.smean(),"")
+  // 	       + "  (" + njm::toString(rs.seMean(),"") + ")");
   
-  rs = r_ma.run(s,ma,numReps,s.fD.finalT,starts);
-  njm::message("         Myopic: "
-	       + njm::toString(rs.smean(),"")
-	       + "  (" + njm::toString(rs.seMean(),"") + ")");
+  // rs = r_ma.run(s,ma,numReps,s.fD.finalT,starts);
+  // njm::message("         Myopic: "
+  // 	       + njm::toString(rs.smean(),"")
+  // 	       + "  (" + njm::toString(rs.seMean(),"") + ")");
   
-  rs = r_ra.run(s,ra,spo,numReps,s.fD.finalT,starts);
-  njm::message("  Policy Search: "
-	       + njm::toString(rs.smean(),"")
-	       + "  (" + njm::toString(rs.seMean(),"") + ")");
+  // rs = r_ra.run(s,ra,spo,numReps,s.fD.finalT,starts);
+  // njm::message("  Policy Search: "
+  // 	       + njm::toString(rs.smean(),"")
+  // 	       + "  (" + njm::toString(rs.seMean(),"") + ")");
 
   // osspo.tp.N = 100;
   // osspo.tp.jitterScale = 4.0;
   // osspo.name = "M1Ossp_" + njm::toString(osspo.tp.N,"",0,0)
   // 	+ "_" + njm::toString(osspo.tp.jitterScale,"",0,0);
-  rs = r_oa.run(s,oa,osspo,numReps,s.fD.finalT,starts);
-  njm::message("One Step Polish: "
-  		   + njm::toString(rs.smean(),"")
-  		   + "  (" + njm::toString(rs.seMean(),"") + ")");
+  // rs = r_oa.run(s,oa,osspo,numReps,s.fD.finalT,starts);
+  // njm::message("One Step Polish: "
+  // 		   + njm::toString(rs.smean(),"")
+  // 		   + "  (" + njm::toString(rs.seMean(),"") + ")");
   
   // osspo.tp.N = 100;
   // osspo.tp.jitterScale = 1.0;
