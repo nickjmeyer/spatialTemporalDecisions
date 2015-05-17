a986ab34290faab9a24785382e6867a4781ab3fb
Nicholas J Meyer <nick.j.meyer@gmail.com>
2015-05-17 15:11:28 -0700

    Add back in distance power

--- src/tuneGen.cpp
+++ src/tuneGen.cpp
@@ -67,29 +67,29 @@
 
 
 
-// double getDPow(const double & power, const double & alpha,
-// 	       const std::vector<double> & caves){
-//   // double meanCaves = std::accumulate(caves.begin(),caves.end(),0);
-//   // meanCaves /= double(caves.size());
+double getDPow(const double & power, const double & alpha,
+	       const std::vector<double> & caves){
+  double meanCaves = std::accumulate(caves.begin(),caves.end(),0);
+  meanCaves /= double(caves.size());
 
-//   // double dPow = std::log(2.0)*std::pow(meanCaves,2.0*power)/alpha + 1.0;
-//   // dPow = std::log(dPow);
-//   // dPow /= std::log(2.0);
+  double dPow = std::log(2.0)*std::pow(meanCaves,2.0*power)/alpha + 1.0;
+  dPow = std::log(dPow);
+  dPow /= std::log(2.0);
 
-//   // return(dPow);
-//   return(1.0);
-// }
-
-
-// void rescaleD(const double & pastScale, const double & currScale,
-// 	      std::vector<double> & d){
-//   double scale = currScale/pastScale;
-//   std::for_each(d.begin(),d.end(),[&scale](double & x){x=std::pow(x,scale);});
-// }
+  return(dPow);
+  // return(1.0);
+}
 
 
+void rescaleD(const double & pastScale, const double & currScale,
+	      std::vector<double> & d){
+  double scale = currScale/pastScale;
+  std::for_each(d.begin(),d.end(),[&scale](double & x){x=std::pow(x,scale);});
+}
 
-template <class S, class NT,class RN>
+
+
+template <class S, class NT,class RN, class MG>
 double TuneGenNT(S & s, const int numReps, const Starts & starts){
   NT nt;
   RN rn;
@@ -100,19 +100,22 @@ double TuneGenNT(S & s, const int numReps, const Starts & starts){
   int numYears = s.fD.finalT;
   double tol = 0.01;
 
-  // std::vector<double> scaleD;
-  // njm::fromFile(scaleD, njm::sett.srcExt("gDistRaw.txt"));
-  // double pastScale = 1.0;
-  // double currScale = getDPow(getPower(s.modelGen_r,s.fD),
-  // 			     getAlpha(s.modelGen_r,s.fD),
-  // 			     s.fD.caves);
-
-  // rescaleD(pastScale,currScale,scaleD);
-  // s.fD.gDist = scaleD;
+  std::vector<double> scaleD;
+  njm::fromFile(scaleD, njm::sett.srcExt("gDistRaw.txt"));
+  double pastScale = 1.0;
+  double currScale = getDPow(s.modelGen_r.getPar({"power"})[0],
+  			     s.modelGen_r.getPar({"alpha"})[0],
+  			     s.fD.caves);
 
   std::vector<double> par = s.modelGen_r.getPar();
+  rescaleD(pastScale,currScale,scaleD);
+  s.fD.gDist = scaleD;
+  s.preCompData();
+  s.modelGen_r = MG(s.fD);
+  s.modelGen_r.putPar(par.begin());
+  s.modelEst_r = MG(s.fD);
   s.modelEst_r.putPar(par.begin());
-  // double power = getPower(s.modelGen_r,s.fD);
+  
   double val = rn.run(s,nt,numReps,numYears,starts).smean();
   double scale = 1.025, shrink = .9;
   int above = int(val > goal);
@@ -146,19 +149,23 @@ double TuneGenNT(S & s, const int numReps, const Starts & starts){
     par = s.modelGen_r.getPar();
     s.modelEst_r.putPar(par.begin());
 
-    // s.modelGen_r.putPar(par.begin());
-    // putPower(power,s.modelGen_r,s.fD);
-    // s.modelEst_r.putPar(par.begin());
-    // putPower(power,s.modelEst_r,s.fD);
-
     s.revert();
 
-    // pastScale = currScale;
-    // currScale = getDPow(getPower(s.modelGen_r,s.fD),
-    // 			getAlpha(s.modelGen_r,s.fD),
-    // 			s.fD.caves);
-    // rescaleD(pastScale,currScale,scaleD);
-    // s.fD.gDist = scaleD;
+    
+    pastScale = currScale;
+    currScale = getDPow(s.modelGen_r.getPar({"power"})[0],
+			s.modelGen_r.getPar({"alpha"})[0],
+			s.fD.caves);
+
+    par = s.modelGen_r.getPar();
+    rescaleD(pastScale,currScale,scaleD);
+    
+    s.fD.gDist = scaleD;
+    s.preCompData();
+    s.modelGen_r = MG(s.fD);
+    s.modelGen_r.putPar(par.begin());
+    s.modelEst_r = MG(s.fD);
+    s.modelEst_r.putPar(par.begin());
 
 
     val = rn.run(s,nt,numReps,numYears,starts).smean();
@@ -265,16 +272,16 @@ int main(int argc, char ** argv){
   njm::sett.set(argc,argv);
 
   {
-    typedef ModelTimeExpCavesGDist GM;
-    typedef GM EM;
+    typedef ModelTimeExpCavesGDist MG;
+    typedef MG ME;
 
-    typedef System<GM,EM> S;
-    typedef NoTrt<EM> NT;
-    typedef ProximalGDistAgent<EM> PA;
-    typedef MyopicAgent<EM> MA;
+    typedef System<MG,ME> S;
+    typedef NoTrt<ME> NT;
+    typedef ProximalGDistAgent<ME> PA;
+    typedef MyopicAgent<ME> MA;
 
-    typedef ToyFeatures5<EM> F;
-    typedef RankAgent<F,EM> RA;
+    typedef ToyFeatures5<ME> F;
+    typedef RankAgent<F,ME> RA;
 
     typedef VanillaRunnerNS<S,NT> RN;
     typedef VanillaRunnerNS<S,PA> RP;
@@ -298,7 +305,7 @@ int main(int argc, char ** argv){
 
     njm::message("Tuning Intercept");
 
-    double valNT = TuneGenNT<S,NT,RN>(s,numReps,starts);
+    double valNT = TuneGenNT<S,NT,RN,MG>(s,numReps,starts);
 
     njm::message("Tuning Treatment");
 
