5251e8010800f0916dd2bfce9a9d06ab2770f24e
Nicholas J Meyer <nick.j.meyer@gmail.com>
2015-06-13 11:51:27 -0700

    Add treat all agent

---
+++ src/allAgent.hpp
@@ -0,0 +1,26 @@
+#ifndef ALL_AGENT_HPP__
+#define ALL_AGENT_HPP__
+
+
+#include <armadillo>
+#include <vector>
+#include <queue>
+#include "data.hpp"
+#include "model.hpp"
+#include "agent.hpp"
+#include "features.hpp"
+
+template <class M>
+class AllAgent : public BaseAgent<M> {
+ public:
+  virtual void applyTrt(const SimData & sD,
+			TrtData & tD,
+			const FixedData & fD,
+			const DynamicData & dD,
+			M & model);
+
+  static std::string name;
+};
+
+
+#endif
