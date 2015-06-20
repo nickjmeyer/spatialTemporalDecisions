0f345078b15c23f1526a976e6b701480315e7bf4
Nicholas J Meyer <nick.j.meyer@gmail.com>
2015-06-19 18:12:17 -0700

    Ready to test

--- src/test.cpp
+++ src/test.cpp
@@ -14,38 +14,38 @@ int main(int argc, char ** argv){
   typedef ProximalGDistAgent<ME> PA;
   typedef MyopicAgent<ME> MA;
 
-  typedef ToyFeatures5<ME> F5;
-  typedef RankAgent<F5,ME> RA5;
+  // typedef ToyFeatures5<ME> F5;
+  // typedef RankAgent<F5,ME> RA5;
 
-  typedef M1SpOptim<S,RA5,ME> SPO5;
+  // typedef M1SpOptim<S,RA5,ME> SPO5;
   
   typedef VanillaRunner<S,NT> R_NT;
   typedef VanillaRunner<S,PA> R_PA;
   typedef FitOnlyRunner<S,MA> R_MA;
-  typedef OptimRunner<S,RA5,SPO5> R_RA5;
+  // typedef OptimRunner<S,RA5,SPO5> R_RA5;
 
   S s;
   s.modelGen_r.setType(MLES);
   s.modelEst_r.setType(MLES);
 
-  int numReps = 96;
+  int numReps = 4;
   Starts starts(numReps,s.fD.numNodes);
 
   NT nt;
   PA pa;
   MA ma;
-  RA5 ra5;
+  // RA5 ra5;
 
-  ra5.name = "rank_5";
-  ra5.tp.jitterScale = -1;
+  // ra5.name = "rank_5";
+  // ra5.tp.jitterScale = -1;
 
-  SPO5 spo5;
-  spo5.tp.fixSample = 1;
+  // SPO5 spo5;
+  // spo5.tp.fixSample = 1;
   
   R_NT r_nt;
   R_PA r_pa;
   R_MA r_ma;
-  R_RA5 r_ra5;
+  // R_RA5 r_ra5;
 
   RunStats rs;
 
@@ -64,10 +64,11 @@ int main(int argc, char ** argv){
   	       + njm::toString(rs.smean(),"")
   	       + "  (" + njm::toString(rs.seMean(),"") + ")");
   
-  rs = r_ra5.run(s,ra5,spo5,numReps,s.fD.finalT,starts);
-  njm::message("  Policy Search: "
-  	       + njm::toString(rs.smean(),"")
-  	       + "  (" + njm::toString(rs.seMean(),"") + ")");
+  // rs = r_ra5.run(s,ra5,spo5,numReps,s.fD.finalT,starts);
+  // njm::message("  Policy Search: "
+  // 	       + njm::toString(rs.smean(),"")
+  // 	       + "  (" + njm::toString(rs.seMean(),"") + ")");
 
+  njm::sett.clean();
   return 0;
 }
