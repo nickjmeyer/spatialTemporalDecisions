2c032d58ffaacbc3077132582f6014dda960e581
Nicholas J Meyer <nick.j.meyer@gmail.com>
2015-06-28 18:43:53 -0700

    Fixed bug

--- src/getCov.cpp
+++ src/getCov.cpp
@@ -22,76 +22,13 @@ std::vector<double> getCovCpp(const std::vector<double> & rv,
 			      const int p);
 
 // [[Rcpp::export]]
-void getSigmaSparse(arma::mat & sigma,
-		    const std::vector<double> & cenX,
-		    const std::vector<double> & cenY,
-		    const int n,
-		    const double rho,
-		    const double tau,
-		    const double eta,
-		    const int p);
-
-
-// int main(){
-//   std::ifstream ifs;
-//   ifs.open("eta.txt");
-//   double eta;
-//   ifs >> eta;
-//   ifs.close();
-
-//   ifs.open("n.txt");
-//   int n;
-//   ifs >> n;
-//   ifs.close();
-
-//   n = 1000;
-
-//   ifs.open("p.txt");
-//   int p;
-//   ifs >> p;
-//   ifs.close();
-
-//   ifs.open("rho.txt");
-//   double rho;
-//   ifs >> rho;
-//   ifs.close();
-
-//   ifs.open("rv.txt");
-//   std::vector<double> rv;
-//   for(int i = 0; i < n*p; ++i){
-//     double val;
-//     ifs >> val;
-//     rv.push_back(val);
-//   }
-//   ifs.close();
-
-//   ifs.open("tau.txt");
-//   double tau;
-//   ifs >> tau;
-//   ifs.close();
-
-//   ifs.open("x.txt");
-//   std::vector<double> x;
-//   for(int i = 0; i < n; ++i){
-//     double val;
-//     ifs >> val;
-//     x.push_back(val);
-//   }
-//   ifs.close();
-
-//   ifs.open("y.txt");
-//   std::vector<double> y;
-//   for(int i = 0; i < n; ++i){
-//     double val;
-//     ifs >> val;
-//     y.push_back(val);
-//   }
-//   ifs.close();
-
-//   std::vector<double> cov = getCovCpp(rv,x,y,n,rho,tau,eta,p);
-
-//   return 0;
-// }
+arma::mat getSigmaSparse(const std::vector<double> & cenX,
+			 const std::vector<double> & cenY,
+			 const int n,
+			 const double rho,
+			 const double tau,
+			 const double eta,
+			 const int p);
 
 
 std::vector<double> getCovCpp(const std::vector<double> & rv,
@@ -103,7 +40,7 @@ std::vector<double> getCovCpp(const std::vector<double> & rv,
 			      const double eta,
 			      const int p){
   arma::mat sigma;
-  getSigmaSparse(sigma,cenX,cenY,n,rho,tau,eta,p);
+  sigma = getSigmaSparse(cenX,cenY,n,rho,tau,eta,p);
 
 
   arma::colvec rvArma(rv);
@@ -115,18 +52,18 @@ std::vector<double> getCovCpp(const std::vector<double> & rv,
 
 
 
-void getSigmaSparse(arma::mat & sigma,
-		    const std::vector<double> & cenX,
-		    const std::vector<double> & cenY,
-		    const int n,
-		    const double rho,
-		    const double tau,
-		    const double eta,
-		    const int p){
+arma::mat getSigmaSparse(const std::vector<double> & cenX,
+			 const std::vector<double> & cenY,
+			 const int n,
+			 const double rho,
+			 const double tau,
+			 const double eta,
+			 const int p){
   int i,j,k,l,ind0,ind1;
   double val;
   double dist;
 
+  arma::mat sigma;
   sigma.resize(n*p,n*p);
   sigma.zeros();
 
@@ -145,4 +82,5 @@ void getSigmaSparse(arma::mat & sigma,
       }
     }
   }
+  return(sigma);
 }
