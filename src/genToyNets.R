9b3fca47473d12c71103edb006fa1d1274b2537a
Nicholas J Meyer <nick.j.meyer@gmail.com>
2015-06-28 19:14:38 -0700

    Revert "Individual worker loads package"
    This reverts commit c849f61d8e3f394623543c2a25db53d79be2b9dc.

--- src/genToyNets.R
+++ src/genToyNets.R
@@ -1,34 +1,6 @@
 rm(list=ls(all=TRUE))
 
-
-generateNets <- function(n,display=TRUE){
-  nets = c("alleyNet","bowTieNet","gridNet","randNet",
-           "scaleFreeNet")
-  nets = foreach(net = nets)%dopar%{
-    source("makeToyNets.R")
-
-    argGen = get(paste(net,"Args",sep=""))
-    netGen = get(paste("gen",
-                       paste(toupper(substring(net,1,1)),
-                             substring(net,2),sep=""),sep=""))
-
-    netRes = do.call(netGen,argGen(n))
-    cat(paste(net,"is done","\n"))
-    return(netRes)
-  }
-
-  if(display){
-    for(net in nets){
-      plotNet(net)
-      readline(prompt="Press [enter] to continue.")
-    }
-    dev.off()
-  }
-
-  return(nets)
-}
-
-
+source("makeToyNets.R")
 
 netSize = as.numeric(commandArgs(TRUE)[1])
 nets = generateNets(netSize,FALSE)
