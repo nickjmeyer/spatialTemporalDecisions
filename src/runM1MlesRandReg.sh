cea30a7aff249b5733d1bbbc7b56fd82d97b1959
Nicholas J Meyer <nick.j.meyer@gmail.com>
2015-07-01 22:04:33 -0700

    Ready to run all networks

--- src/runM1MlesRandReg.sh
+++ src/runM1MlesRandReg.sh
@@ -3,12 +3,14 @@
 cd ../bin
 
 NETS=("rand")
-SIZE=("100" "500" "1000")
+SIZE=("100" "500" "1000" "10000")
 
 for i in ${NETS[@]}
 do
     for j in ${SIZE[@]}
     do
-	submit ./runM1Mles "../data/toy/${i}${j}" y;
+	../bin/tuneGen "../data/toy/${i}${j}" y;
+	submit ../bin/runM1Mles "../data/toy/${i}${j}" y;
+	submit ../bin/runM1MlesMiss "../data/toy/${i}${j}" y;
     done
 done
