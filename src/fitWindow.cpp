c1e2a6bd9121707d06334a52fce500f57511a642
Nicholas J Meyer <nick.j.meyer@gmail.com>
2015-07-05 19:02:12 -0700

    Added extended gravity model to fitWindow.cpp

--- src/fitWindow.cpp
+++ src/fitWindow.cpp
@@ -10,13 +10,15 @@ void fitWindow(const std::string & ext,
 
   S sObs("obsData_"+ext+".txt");
 
+  std::string name = sObs.modelGen_r.name;
+
   sObs.modelGen_r.mcmc.load(sObs.sD.history,sObs.sD.status,sObs.fD);
   sObs.modelGen_r.mcmc.sample(numSamples,numBurn);
 
   // posterior mean
   sObs.modelGen_r.mcmc.samples.setMean();
   njm::toFile(njm::toString(sObs.modelGen_r.mcmc.samples.getPar()," ","\n"),
-	      njm::sett.datExt("postMean_"+ext+"_",".txt"));
+	      njm::sett.datExt(name + "_postMean_"+ext+"_",".txt"));
 
   // mcmc samples
   int i;
@@ -24,57 +26,73 @@ void fitWindow(const std::string & ext,
     sObs.modelGen_r.mcmc.samples.setPar(i);
     if(i)
       njm::toFile(njm::toString(sObs.modelGen_r.mcmc.samples.getPar()," ","\n"),
-		  njm::sett.datExt("samples_"+ext+"_",".txt"),
+		  njm::sett.datExt(name+"_samples_"+ext+"_",".txt"),
 		  std::ios_base::app);
     else
       njm::toFile(njm::toString(sObs.modelGen_r.mcmc.samples.getPar()," ","\n"),
-		  njm::sett.datExt("samples_"+ext+"_",".txt"),
+		  njm::sett.datExt(name+"_samples_"+ext+"_",".txt"),
 		  std::ios_base::out);
   }
 
   // likelihood
   njm::toFile(njm::toString(sObs.modelGen_r.mcmc.samples.ll,"\n",""),
-  	      njm::sett.datExt("ll_"+ext+"_",".txt"));
+  	      njm::sett.datExt(name+"_ll_"+ext+"_",".txt"));
 
   // likelihood at mean
   njm::toFile(njm::toString(sObs.modelGen_r.mcmc.samples.llPt,"\n"),
-  	      njm::sett.datExt("llPt_"+ext+"_",".txt"));
+  	      njm::sett.datExt(name+"_llPt_"+ext+"_",".txt"));
 
   // pD
   njm::toFile(njm::toString(sObs.modelGen_r.mcmc.samples.pD,"\n"),
-  	      njm::sett.datExt("pD_"+ext+"_",".txt"));
+  	      njm::sett.datExt(name+"_pD_"+ext+"_",".txt"));
 
   // Dbar
   njm::toFile(njm::toString(sObs.modelGen_r.mcmc.samples.Dbar,"\n"),
-  	      njm::sett.datExt("Dbar_"+ext+"_",".txt"));
+  	      njm::sett.datExt(name+"_Dbar_"+ext+"_",".txt"));
 
   // DIC
   njm::toFile(njm::toString(sObs.modelGen_r.mcmc.samples.DIC,"\n"),
-  	      njm::sett.datExt("DIC_"+ext+"_",".txt"));
+  	      njm::sett.datExt(name+"_DIC_"+ext+"_",".txt"));
 
   sObs.modelGen_r.setType(MLE);
   sObs.modelGen_r.fit(sObs.sD,sObs.tD,sObs.fD,sObs.dD,0);
   njm::toFile(njm::toString(sObs.modelGen_r.getPar()," ","\n"),
-	      njm::sett.datExt("MLE_"+ext+"_",".txt"));
+	      njm::sett.datExt(name+"_MLE_"+ext+"_",".txt"));
 }
 
 
 int main(int argc, char ** argv){
   njm::sett.set(argc,argv);
 
-  typedef ModelGravityGDist M;
+  {
+    typedef ModelGravityGDist M;
 
-  int i;
-  // note can't put const variables in shared
-  const int I = 6, win = 3, numSamples = 20000, numBurn = 10000;
+    int i;
+    // note can't put const variables in shared
+    const int I = 6, win = 3, numSamples = 20000, numBurn = 10000;
 #pragma omp parallel for num_threads(omp_get_max_threads())	\
   private(i)
-  for(i = 0; i < I; ++i){
-    std::string file = (njm::toString(i,"",0,0)
-			+ "-"
-			+ njm::toString(i+win-1,"",0,0));
-    fitWindow<M>(file,numSamples,numBurn);
+    for(i = 0; i < I; ++i){
+      std::string file = (njm::toString(i,"",0,0)
+			  + "-"
+			  + njm::toString(i+win-1,"",0,0));
+      fitWindow<M>(file,numSamples,numBurn);
+    }
   }
+  {
+    typedef ModelTimeExpCavesGDistTrendPowCon M;
 
+    int i;
+    // note can't put const variables in shared
+    const int I = 6, win = 3, numSamples = 20000, numBurn = 10000;
+#pragma omp parallel for num_threads(omp_get_max_threads())	\
+  private(i)
+    for(i = 0; i < I; ++i){
+      std::string file = (njm::toString(i,"",0,0)
+			  + "-"
+			  + njm::toString(i+win-1,"",0,0));
+      fitWindow<M>(file,numSamples,numBurn);
+    }
+  }
   return 0;
 }
