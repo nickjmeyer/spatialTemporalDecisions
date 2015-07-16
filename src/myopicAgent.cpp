4b00d968e48941c3987b1d3b2acc63693dce7ab5
Nicholas J Meyer <nick.j.meyer@gmail.com>
2015-07-15 21:41:37 -0700

    Set myopic to do prob of infecting at least one

--- src/myopicAgent.cpp
+++ src/myopicAgent.cpp
@@ -33,24 +33,36 @@ void MyopicAgent<M>::applyTrt(const SimData & sD,
 
   int i,j,node0,total,count;
 
+  // m.modFill(sD,tD,fD,dD);
+  // m.infProbs(sD,tD,fD,dD);
+
+  // notFeat = arma::conv_to<arma::colvec>::from(m.infProbs());
+
+  // infFeat.zeros(sD.numInfected);
+  // for(i=0; i<sD.numInfected; i++){
+  //   node0 = sD.infected.at(i);
+  //   count=0;
+  //   total=0;
+  //   for(j=0; j<sD.numNotInfec; j++){
+  //     if(fD.network.at(node0*fD.numNodes + sD.notInfec.at(j))){
+  // 	total+=notFeat(j);
+  // 	count++;
+  //     }
+  //   }
+  //   infFeat(i) = total/((double)count);
+  // }
+
+
+  ////////////////////////////////////////
   m.modFill(sD,tD,fD,dD);
-  m.infProbs(sD,tD,fD,dD);
+  m.setQuick(sD,tD,fD,dD);
 
-  notFeat = arma::conv_to<arma::colvec>::from(m.infProbs());
+  arma::mat weightMat(m.getQuick().data(),sD.numInfected,sD.numNotInfec,false);
 
-  infFeat.zeros(sD.numInfected);
-  for(i=0; i<sD.numInfected; i++){
-    node0 = sD.infected.at(i);
-    count=0;
-    total=0;
-    for(j=0; j<sD.numNotInfec; j++){
-      if(fD.network.at(node0*fD.numNodes + sD.notInfec.at(j))){
-	total+=notFeat(j);
-	count++;
-      }
-    }
-    infFeat(i) = total/((double)count);
-  }
+  infFeat = 1 - arma::prod(weightMat,1);
+  notFeat = 1 - arma::prod(weightMat,0).t();
+  ////////////////////////////////////////
+
 
   std::priority_queue<std::pair<double,int> > sortInfected,sortNotInfec;
 
