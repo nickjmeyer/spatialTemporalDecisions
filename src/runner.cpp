3fe076d27c3b8ff7a2910f11bd61414bdc866269
Nicholas J Meyer <nick.j.meyer@gmail.com>
2015-07-20 00:11:18 -0700

    Testing other model

--- src/runner.cpp
+++ src/runner.cpp
@@ -437,6 +437,11 @@ VanillaRunnerNS<System<ModelTimeExpCavesGPowGDistTrendPowCon,
 		AllAgent<ModelTimeExpCavesGPowGDistTrendPowCon> >;
 
 template class
+VanillaRunnerNS<System<ModelTimeExpCavesGDist,
+		       ModelTimeExpCavesGDist>,
+		AllAgent<ModelTimeExpCavesGDist> >;
+
+template class
 VanillaRunnerNS<System<ModelTimeExpCavesGDistTrendPowCon,
 		       ModelTimeExpCavesGDistTrendPowCon>,
 		ProximalGDistAgent<ModelTimeExpCavesGDistTrendPowCon> >;
@@ -512,6 +517,12 @@ VanillaRunnerNS<System<ModelTimeExpCavesGDistTrendPowCon,
 			  ModelTimeExpCavesGDistTrendPowCon> >;
 
 template class
+VanillaRunnerNS<System<ModelTimeExpCavesGDist,
+		       ModelTimeExpCavesGDist>,
+		RankAgent<WnsFeatures3<ModelTimeExpCavesGDist>,
+			  ModelTimeExpCavesGDist> >;
+
+template class
 VanillaRunnerNS<System<ModelTimeExpCavesGPowGDistTrendPowCon,
 		       ModelTimeExpCavesGPowGDistTrendPowCon>,
 		RankAgent<ToyFeatures5<ModelTimeExpCavesGPowGDistTrendPowCon>,
@@ -531,11 +542,6 @@ template class VanillaRunnerNS<System<ModelGravityGDist,
 				      ModelGravityGDist>,
 			       RankAgent<WnsFeatures3<ModelGravityGDist>,
 					 ModelGravityGDist> >;
-
-template class VanillaRunnerNS<System<ModelTimeExpCavesGDist,
-				      ModelTimeExpCavesGDist>,
-			       RankAgent<WnsFeatures3<ModelTimeExpCavesGDist>,
-					 ModelTimeExpCavesGDist> >;
 
 template class VanillaRunnerNS<System<ModelTimeExpCavesGDist,
 				      ModelTimeExpCavesGDist>,
