06c8e66e595a2d312ced3fc0604f0c29593c4092
Nicholas J Meyer <nick.j.meyer@gmail.com>
2015-08-04 00:22:05 -0400

    Increase samples

--- src/bayesP.cpp
+++ src/bayesP.cpp
@@ -135,7 +135,7 @@ void runBayesP(const std::string & file, const int obs,
     sObs.modelGen_r.mcmc.samples.setMean();
     std::vector<double> par = sObs.modelGen_r.mcmc.samples.getPar();
     sObs.modelGen_r.putPar(par.begin());
-    sObs.modelGen_r.save();
+    // sObs.modelGen_r.save();
 
     std::vector< std::vector<double> > stats;
 
@@ -308,9 +308,9 @@ void runBayesP(const std::string & file, const int obs,
 int main(int argc, char ** argv){
   njm::sett.set(argc,argv);
 
-  // int numSamples = 1000000, numBurn = 25000, numStats = 50000;
+  int numSamples = 1000000, numBurn = 25000, numStats = 50000;
   // int numSamples = 50000, numBurn = 25000, numStats = 10000;
-  int numSamples = 20000, numBurn = 10000, numStats = 10000;
+  // int numSamples = 20000, numBurn = 10000, numStats = 10000;
   // int numSamples = 100, numBurn = 50, numStats = 50;
   // int numSamples = 10, numBurn = 5, numStats = 5;
 
