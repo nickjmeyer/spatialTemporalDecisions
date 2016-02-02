2dd228b3d594b3b1489b22cb3baafd77c9b702b8
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-02-02 09:20:20 -0500

    Debugging stuff

--- src/model.cpp
+++ src/model.cpp
@@ -290,6 +290,8 @@ std::vector<double> ModelBase::partial(const int notNode,
     pi = pars[i]->partial(notNode,infNode,sD,tD,fD,dD);
     p.insert(p.end(),pi.begin(),pi.end());
   }
+
+  // std::cout << "p: " << njm::toString(p) << std::endl;
   return p;
 }
 
@@ -329,6 +331,14 @@ std::vector<double> ModelBase::partial2(const int notNode,
 
   }
 
+  // std::cout << totLen << std::endl;
+  // std::cout << p[9*totLen + 9] << ", "
+  // 	    << p[9*totLen + 10] << ", " << p[9*totLen + 11] << std::endl;
+  // std::cout << p[10*totLen + 9] << ", "
+  // 	    << p[10*totLen + 10] << ", " << p[10*totLen + 11] << std::endl;
+  // std::cout << p[11*totLen + 9] << ", "
+  // 	    << p[11*totLen + 10] << ", " << p[11*totLen + 11] << std::endl;
+
   return p;
 }
 
@@ -337,10 +347,12 @@ void ModelBase::setFisher(const SimData & sD,
 			  const TrtData & tD,
 			  const FixedData & fD,
 			  const DynamicData & dD){
+  std::cout << "=========== fisher ============" << std::endl;
   fisher.resize(numPars * numPars);
   std::fill(fisher.begin(),fisher.end(),0);
 
-
+  std::cout << njm::toString(getPar()) << std::endl;
+  std::cout << "power: " << getPar({"power"})[0] << std::endl;
 
   std::vector<std::vector<int> > hist = sD.history;
   hist.push_back(sD.status);
@@ -353,9 +365,38 @@ void ModelBase::setFisher(const SimData & sD,
     TrtData tDi= std::get<1>(db[t]);
     DynamicData dDi = std::get<2>(db[t]);
 
+    std::cout << "t = " << t << std::endl;
+
+    std::cout << "fill" << std::endl;
     setFill(sDi,tDi,fD,dDi);
+    std::cout << "end fill" << std::endl;
     setQuick(sDi,tDi,fD,dDi);
     infProbs(sDi,tDi,fD,dDi);
+
+    if(t == 0){
+      std::cout << "infected: " << std::endl;
+      std::cout << sDi.numInfected << std::endl;
+      std::cout << njm::toString(sDi.infected) << std::endl;
+
+      std::cout << "quick: " << std::endl;
+      std::cout << njm::toString(quick) << std::endl;
+
+      std::cout << "infProbs: " << std::endl;
+      std::cout << njm::toString(expitInfProbs) << std::endl;
+
+      std::cout << "neighbors of " << sDi.infected.at(0) << ": " << std::endl;
+      for(int i = 0; i < fD.numNodes; ++i){
+	if(fD.network.at(i*fD.numNodes + sDi.infected.at(0)) == 1){
+	  std::cout << i << ": " << probs[i*fD.numNodes + sDi.infected.at(0)]
+		    << " ++ " << fD.gDist[i*fD.numNodes + sDi.infected.at(0)]
+		    << std::endl;
+	}
+      }
+      std::cout << std::endl;
+
+      // std::cout << "probs: " << std::endl;
+      // std::cout << njm::toString(probs) << std::endl;
+    }
 
     for(nN = 0; nN < sDi.numNotInfec; ++nN){
       std::vector<double> dbl(numPars*numPars,0);
@@ -435,6 +476,10 @@ void ModelBase::setFisher(const SimData & sD,
   // 	    << std::endl;
   Eigen::LDLT<Eigen::MatrixXd> ldlt(-I);
 
+  for(int i = 0; i < int(I.rows()); ++i){
+    std::cout << "I row " << i << ": " << I.row(i) << std::endl;
+  }
+
   if(ldlt.info() != Eigen::Success){
     static int badCnt = 0;
     std::cout << "Error: ModelBase::getFisher(): eigen solver failed"
@@ -443,15 +488,28 @@ void ModelBase::setFisher(const SimData & sD,
 					njm::toString(badCnt++,"",0,0)));
     throw(1);
   }
+
   else{
+    std::cout << "here" << std::endl;
+    std::cout << I(0,0) << std::endl;
     Eigen::Transpositions<Eigen::Dynamic,Eigen::Dynamic> P;
     P = ldlt.transpositionsP();
     Eigen::MatrixXd L = ldlt.matrixL();
+
+    for(int i = 0; i < int(L.rows()); ++i){
+      std::cout << "L row " << i << ": " << L.row(i) << std::endl;
+    }
+
     // std::cout << "L: " << std::endl
     // 	      << L << std::endl
     // 	      << std::endl;
     L.transposeInPlace();
     Eigen::VectorXd D = ldlt.vectorD();
+
+    std::cout << "D: "  << std::endl << D << std::endl;
+
+    throw(1);
+
     // std::cout << "D: " << std::endl
     // 	      << D << std::endl
     // 	      << std::endl;
