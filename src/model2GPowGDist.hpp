99d87784a465f2455ac7d03ab6609e16a587979e
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-02-02 14:25:58 -0500

    Remove old fit functions

--- src/model2GPowGDist.hpp
+++ src/model2GPowGDist.hpp
@@ -22,33 +22,7 @@ class Model2GPowGDist : public ModelBase {
   Model2GPowGDist(const Model2GPowGDist & m);
 
   virtual Model2GPowGDist & operator=(const Model2GPowGDist & m);
-
-  virtual void fit(const SimData & sD, const TrtData & tD,
-		   const FixedData & fD, const DynamicData & dD,
-		   const int & useInit);
-
-  virtual void fit(const SimData & sD, const TrtData & tD,
-		   const FixedData & fD, const DynamicData & dD,
-		   std::vector<double> pars);
 };
-
-
-class Model2GPowGDistFitData {
- public:
-  Model2GPowGDistFitData(const Model2GPowGDist & m,
-			    const std::vector<double> & all,
-			    const FixedData & fD,
-			    const std::vector<std::vector<int> > & history);
-
-  Model2GPowGDist m;
-  FixedData fD;
-  std::vector< std::vector<int> > history;
-};
-
-
-double modelGPow2GDistFitObjFn (const gsl_vector * x, void * params);
-
-
 
 
 #endif
