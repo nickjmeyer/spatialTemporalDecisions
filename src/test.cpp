ca32bbc14f33154073a9fc0ed9ad88f8035037b5
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-02-02 14:26:36 -0500

    Update test for new functions

--- src/test.cpp
+++ src/test.cpp
@@ -5,30 +5,43 @@
 int main(int argc, char ** argv){
   njm::sett.set(argc,argv);
 
-  typedef Model2GravityGDist M;
+  typedef Model2GPowGDist M;
   typedef System<M,M> S;
 
 
   S s;
   s.modelGen_r.setType(MLES);
   s.modelEst_r.setType(MLES);
+
+  std::vector<std::string> names = {"power"};
+  double power = s.modelGen_r.getPar(names)[0];
+  s.modelGen_r.setPar("power",std::log(power));
+
+  std::vector<double> par = s.modelGen_r.getPar();
+  s.modelEst_r.putPar(par.begin());
   s.revert();
 
-  Starts starts(1,s.fD.numNodes);
-  s.reset(starts[0]);
+  Starts starts(15,s.fD.numNodes);
+  s.reset(starts[3]);
 
 
   int i;
-  for(i = 0; i < 15; ++i){
+  for(i = 0; i < s.fD.trtStart; ++i){
     s.nextPoint();
   }
 
-  s.modelEst.estimateMle(s.sD,s.tD,s.fD,s.dD);
+  std::cout << "numInfected: " << s.sD.numInfected << std::endl;
+
+  s.modelEst.fit(s.sD,s.tD,s.fD,s.dD,false);
 
   std::cout << "gen: " << njm::toString(s.modelGen.getPar()) << std::endl;
   std::cout << "est: " << njm::toString(s.modelEst.getPar()) << std::endl;
 
-  s.modelEst.setFisher(s.sD,s.tD,s.fD,s.dD);
+  for(i = 0; i < 10; i++){
+    s.modelEst.sample();
+    std::cout << i << ": " << njm::toString(s.modelEst.getPar()) << std::endl;
+    std::cout << "==========================================" << std::endl;
+  }
 
   njm::sett.clean();
   return 0;
