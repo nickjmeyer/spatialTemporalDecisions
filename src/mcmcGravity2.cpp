9d5ea7e5528d74f082e0fdcbec5ca7ca15904c5f
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-02-02 15:49:36 -0500

    Turn display on

--- src/mcmcGravity2.cpp
+++ src/mcmcGravity2.cpp
@@ -329,7 +329,7 @@ void Gravity2Mcmc::sample(int const numSamples, int const numBurn,
   double logAlpha_cur,logAlpha_can;
 
   int displayOn=1;
-  int display=0;
+  int display=1;
 
   // do a bunch of nonsense...
   for(i=0; i<numSamples; ++i){
