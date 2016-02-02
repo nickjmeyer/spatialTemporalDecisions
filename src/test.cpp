2fa83119fe9f284c214916198b2e04fde90c54b4
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-02-02 10:55:56 -0500

    Test new MLE functions

--- src/test.cpp
+++ src/test.cpp
@@ -10,37 +10,25 @@ int main(int argc, char ** argv){
 
 
   S s;
+  s.modelGen_r.setType(MLES);
+  s.modelEst_r.setType(MLES);
+  s.revert();
+
   Starts starts(1,s.fD.numNodes);
-  s.modelEst_r = s.modelGen_r;
   s.reset(starts[0]);
 
 
   int i;
-  for(i = 0; i < 2; ++i){
+  for(i = 0; i < 15; ++i){
     s.nextPoint();
   }
 
-  std::cout << s.value() << std::endl;
+  s.modelEst.estimateMle(s.sD,s.tD,s.fD,s.dD);
 
-  RankAgent<ToyFeatures5<M>,M> ra;
+  std::cout << "gen: " << njm::toString(s.modelGen.getPar()) << std::endl;
+  std::cout << "est: " << njm::toString(s.modelEst.getPar()) << std::endl;
 
-  // ra.tp.jitterScale = -1;
-  // ra.tp.shuffle = false;
-
-  int j;
-  for(i = 0; i < 10; ++i){
-    std::fill(s.tD.a.begin(),s.tD.a.end(),0);
-    std::fill(s.tD.p.begin(),s.tD.p.end(),0);
-    ra.applyTrt(s.sD,s.tD,s.fD,s.dD,s.modelGen);
-
-    for(j = 0; j < s.fD.numNodes; ++j){
-      if(s.tD.a.at(j) == 1 || s.tD.p.at(j) == 1){
-	std::cout << j << " ";
-      }
-    }
-    std::cout << std::endl;
-  }
-
+  s.modelEst.setFisher(s.sD,s.tD,s.fD,s.dD);
 
   njm::sett.clean();
   return 0;
