c4387c974a0598cc57595670d3e764cef35bd003
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-09-02 14:26:15 -0400

    add shells for model tests

--- src/test/test_model.cpp
+++ src/test/test_model.cpp
@@ -1,8 +1,76 @@
 #include <gtest/gtest.h>
 #include <glog/logging.h>
 
-TEST(TestModel,Test0) {
-	ASSERT_EQ(0,0);
+TEST(TestModel,TestInit) {
+}
+
+TEST(TestModel,TestRead) {
+}
+
+TEST(TestModel,TestSave) {
+}
+
+TEST(TestModel,TestLinScale) {
+}
+
+TEST(TestModel,TestSetType) {
+}
+
+TEST(TestModel,TestSetFixSample) {
+}
+
+TEST(TestModel,TestGetType) {
+}
+
+TEST(TestModel,TestSetEdgeToEdge) {
+}
+
+TEST(TestModel,TestGetEdgeToEdge) {
+}
+
+TEST(TestModel,TestInfProbs) {
+}
+
+TEST(TestModel,TestRevProbs) {
+}
+
+TEST(TestModel,TestSetFill) {
+}
+
+TEST(TestModel,TestModFill) {
+}
+
+TEST(TestModel,TestSetQuick) {
+}
+
+TEST(TestModel,TestOneOnOne) {
+}
+
+TEST(TestModel,TestGetPar) {
+}
+
+TEST(TestModel,TestPutPar) {
+}
+
+TEST(TestModel,TestSetPar) {
+}
+
+TEST(TestModel,TestPartial) {
+}
+
+TEST(TestModel,TestPartial2) {
+}
+
+TEST(TestModel,TestSetFisher) {
+}
+
+TEST(TestModel,TestSample) {
+}
+
+TEST(TestModel,TestRevert) {
+}
+
+TEST(TestModel,TestFit) {
 }
 
 
