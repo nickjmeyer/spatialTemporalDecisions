bc10d24c7a229c4c24273ff0ef84ea72ac86c8aa
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-09-03 20:50:44 -0400

    start building test case fixture

--- src/test/test_model.cpp
+++ src/test/test_model.cpp
@@ -2,77 +2,105 @@
 #include <glog/logging.h>
 #include <boost/filesystem.hpp>
 #include "settings.hpp"
+#include "data.hpp"
+#include "system.hpp"
+#include "paramIntercept.hpp"
+#include "model.hpp"
 
-TEST(TestModel,TestInit) {
+class TestModel : public ::testing::Test {
+public:
+  ModelBase * m;
+
+  SimData * sD;
+  TrtData * tD;
+  FixedData * fD;
+  DynamicData * dD;
+
+  TestModel() {
+
+
+    // m = new ModelBase("TestModel",{new ParamIntercept},*fD);
+  }
+
+  ~TestModel() {
+    // delete m;
+    // delete sD;
+    // delete tD;
+    // delete fD;
+    // delete dD;
+  }
+};
+
+TEST_F(TestModel,TestInit) {
 }
 
-TEST(TestModel,TestRead) {
+TEST_F(TestModel,TestRead) {
 }
 
-TEST(TestModel,TestSave) {
+TEST_F(TestModel,TestSave) {
 }
 
-TEST(TestModel,TestLinScale) {
+TEST_F(TestModel,TestLinScale) {
 }
 
-TEST(TestModel,TestSetType) {
+TEST_F(TestModel,TestSetType) {
 }
 
-TEST(TestModel,TestSetFixSample) {
+TEST_F(TestModel,TestSetFixSample) {
 }
 
-TEST(TestModel,TestGetType) {
+TEST_F(TestModel,TestGetType) {
 }
 
-TEST(TestModel,TestSetEdgeToEdge) {
+TEST_F(TestModel,TestSetEdgeToEdge) {
 }
 
-TEST(TestModel,TestGetEdgeToEdge) {
+TEST_F(TestModel,TestGetEdgeToEdge) {
 }
 
-TEST(TestModel,TestInfProbs) {
+TEST_F(TestModel,TestInfProbs) {
 }
 
-TEST(TestModel,TestRevProbs) {
+TEST_F(TestModel,TestRevProbs) {
 }
 
-TEST(TestModel,TestSetFill) {
+TEST_F(TestModel,TestSetFill) {
 }
 
-TEST(TestModel,TestModFill) {
+TEST_F(TestModel,TestModFill) {
 }
 
-TEST(TestModel,TestSetQuick) {
+TEST_F(TestModel,TestSetQuick) {
 }
 
-TEST(TestModel,TestOneOnOne) {
+TEST_F(TestModel,TestOneOnOne) {
 }
 
-TEST(TestModel,TestGetPar) {
+TEST_F(TestModel,TestGetPar) {
 }
 
-TEST(TestModel,TestPutPar) {
+TEST_F(TestModel,TestPutPar) {
 }
 
-TEST(TestModel,TestSetPar) {
+TEST_F(TestModel,TestSetPar) {
 }
 
-TEST(TestModel,TestPartial) {
+TEST_F(TestModel,TestPartial) {
 }
 
-TEST(TestModel,TestPartial2) {
+TEST_F(TestModel,TestPartial2) {
 }
 
-TEST(TestModel,TestSetFisher) {
+TEST_F(TestModel,TestSetFisher) {
 }
 
-TEST(TestModel,TestSample) {
+TEST_F(TestModel,TestSample) {
 }
 
-TEST(TestModel,TestRevert) {
+TEST_F(TestModel,TestRevert) {
 }
 
-TEST(TestModel,TestFit) {
+TEST_F(TestModel,TestFit) {
 }
 
 
@@ -92,5 +120,46 @@ int main(int argc, char **argv) {
   pseudoArgv[1] = srcDir;
   pseudoArgv[2] = check;
   njm::sett.set(3,pseudoArgv);
+
+
+  // setup the fake network
+  std::vector<int> fips;
+  fips.push_back(0);
+  fips.push_back(1);
+  fips.push_back(2);
+  fips.push_back(3);
+  njm::toFile(fips,njm::sett.srcExt("fips.txt"));
+
+  std::vector<float> gDist;
+  gDist.push_back(0.);
+  gDist.push_back(1.);
+  gDist.push_back(1.);
+  gDist.push_back(2.);
+
+  gDist.push_back(1.);
+  gDist.push_back(0.);
+  gDist.push_back(2.);
+  gDist.push_back(1.);
+
+  gDist.push_back(1.);
+  gDist.push_back(2.);
+  gDist.push_back(0.);
+  gDist.push_back(1.);
+
+  gDist.push_back(2.);
+  gDist.push_back(1.);
+  gDist.push_back(1.);
+  gDist.push_back(0.);
+  njm::toFile(gDist,njm::sett.srcExt("gDist.txt"));
+
+  std::vector<int> caves;
+  caves.push_back(1);
+  caves.push_back(2);
+  caves.push_back(3);
+  caves.push_back(4);
+  njm::toFile(caves,njm::sett.srcExt("caves.txt"));
+
+
+
   return RUN_ALL_TESTS();
 }
