17fc8998781cf0e4d8687fb93a20f0c0d1d7b36a
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-09-14 16:27:12 -0400

    start adding copy params script

---
+++ src/main/copyParams.cpp
@@ -0,0 +1,12 @@
+#include "system.hpp"
+#include "model.hpp"
+#include "model2GravityEDist.hpp"
+#include "model2EdgeToEdge.hpp"
+
+template <typename T>
+copyParams<T>(const boost::filesystem path) {
+}
+
+
+int main() {
+}
