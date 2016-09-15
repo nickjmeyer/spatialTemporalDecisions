0059c19aeea5eaf056df3212435eb71a11454a8c
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-09-15 12:07:31 -0400

    add runner script

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,139 +2,144 @@
 
 PROJ_ROOT=.
 
-EXEC_PATH=$(PROJ_ROOT)/build/main
+EXEC_PATH=${PROJ_ROOT}/build/main
 
-WNS=$(PROJ_ROOT)/data/wns
-TOY=$(PROJ_ROOT)/data/toy
+WNS=${PROJ_ROOT}/data/wns
+TOY=${PROJ_ROOT}/data/toy
 
-$(EXEC_PATH)/tuneGenWNS --srcDir $(PROJ_ROOT)/data/wns --noedgeToEdge
-$(EXEC_PATH)/tuneGenWNS --srcDir $(PROJ_ROOT)/data/wns --edgeToEdge
+DRY=--nodryRun
+
+EDGE_TO_EDGE="--edgeToEdge ${DRY}"
+SPATIAL="${SPATIAL} ${DRY}"
+
+${EXEC_PATH}/tuneGenWNS --srcDir ${PROJ_ROOT}/data/wns ${SPATIAL} ${DRY}
+${EXEC_PATH}/tuneGenWNS --srcDir ${PROJ_ROOT}/data/wns --edgeToEdge ${DRY}
 
 ########################################
 ## copy params
 
 ## spatial spread
-$(EXEC_PATH)/copyParams --srcDir $(WNS) --outDir $(TOY)/crp100 --noedgeToEdge
-$(EXEC_PATH)/copyParams --srcDir $(WNS) --outDir $(TOY)/crp500 --noedgeToEdge
-$(EXEC_PATH)/copyParams --srcDir $(WNS) --outDir $(TOY)/crp1000 --noedgeToEdge
+${EXEC_PATH}/copyParams --srcDir ${WNS} --outDir ${TOY}/crp100 ${SPATIAL}
+${EXEC_PATH}/copyParams --srcDir ${WNS} --outDir ${TOY}/crp500 ${SPATIAL}
+${EXEC_PATH}/copyParams --srcDir ${WNS} --outDir ${TOY}/crp1000 ${SPATIAL}
 
-$(EXEC_PATH)/copyParams --srcDir $(WNS) --outDir $(TOY)/grid100 --noedgeToEdge
-$(EXEC_PATH)/copyParams --srcDir $(WNS) --outDir $(TOY)/grid500 --noedgeToEdge
-$(EXEC_PATH)/copyParams --srcDir $(WNS) --outDir $(TOY)/grid1000 --noedgeToEdge
+${EXEC_PATH}/copyParams --srcDir ${WNS} --outDir ${TOY}/grid100 ${SPATIAL}
+${EXEC_PATH}/copyParams --srcDir ${WNS} --outDir ${TOY}/grid500 ${SPATIAL}
+${EXEC_PATH}/copyParams --srcDir ${WNS} --outDir ${TOY}/grid1000 ${SPATIAL}
 
-$(EXEC_PATH)/copyParams --srcDir $(WNS) --outDir $(TOY)/rand100 --noedgeToEdge
-$(EXEC_PATH)/copyParams --srcDir $(WNS) --outDir $(TOY)/rand500 --noedgeToEdge
-$(EXEC_PATH)/copyParams --srcDir $(WNS) --outDir $(TOY)/rand1000 --noedgeToEdge
+${EXEC_PATH}/copyParams --srcDir ${WNS} --outDir ${TOY}/rand100 ${SPATIAL}
+${EXEC_PATH}/copyParams --srcDir ${WNS} --outDir ${TOY}/rand500 ${SPATIAL}
+${EXEC_PATH}/copyParams --srcDir ${WNS} --outDir ${TOY}/rand1000 ${SPATIAL}
 
 ## network spread
-$(EXEC_PATH)/copyParams --srcDir $(WNS) --outDir $(TOY)/grid100 --edgeToEdge
-$(EXEC_PATH)/copyParams --srcDir $(WNS) --outDir $(TOY)/grid500 --edgeToEdge
-$(EXEC_PATH)/copyParams --srcDir $(WNS) --outDir $(TOY)/grid1000 --edgeToEdge
+${EXEC_PATH}/copyParams --srcDir ${WNS} --outDir ${TOY}/grid100 ${EDGE_TO_EDGE}
+${EXEC_PATH}/copyParams --srcDir ${WNS} --outDir ${TOY}/grid500 ${EDGE_TO_EDGE}
+${EXEC_PATH}/copyParams --srcDir ${WNS} --outDir ${TOY}/grid1000 ${EDGE_TO_EDGE}
 
-$(EXEC_PATH)/copyParams --srcDir $(WNS) --outDir $(TOY)/rand100 --edgeToEdge
-$(EXEC_PATH)/copyParams --srcDir $(WNS) --outDir $(TOY)/rand500 --edgeToEdge
-$(EXEC_PATH)/copyParams --srcDir $(WNS) --outDir $(TOY)/rand1000 --edgeToEdge
+${EXEC_PATH}/copyParams --srcDir ${WNS} --outDir ${TOY}/rand100 ${EDGE_TO_EDGE}
+${EXEC_PATH}/copyParams --srcDir ${WNS} --outDir ${TOY}/rand500 ${EDGE_TO_EDGE}
+${EXEC_PATH}/copyParams --srcDir ${WNS} --outDir ${TOY}/rand1000 ${EDGE_TO_EDGE}
 
-$(EXEC_PATH)/copyParams --srcDir $(WNS) --outDir $(TOY)/scalefree100 \
-						--noedgeToEdge
-$(EXEC_PATH)/copyParams --srcDir $(WNS) --outDir $(TOY)/scalefree500 \
-						--noedgeToEdge
-$(EXEC_PATH)/copyParams --srcDir $(WNS) --outDir $(TOY)/scalefree1000 \
-						--noedgeToEdge
+${EXEC_PATH}/copyParams --srcDir ${WNS} --outDir ${TOY}/scalefree100 \
+						${SPATIAL}
+${EXEC_PATH}/copyParams --srcDir ${WNS} --outDir ${TOY}/scalefree500 \
+						${SPATIAL}
+${EXEC_PATH}/copyParams --srcDir ${WNS} --outDir ${TOY}/scalefree1000 \
+						${SPATIAL}
 
 
 ########################################
 ## tune generative models
 
 ## spatial spread
-$(EXEC_PATH)/tuneGen --srcDir $(WNS) --noedgeToEdge
-$(EXEC_PATH)/tuneGen --srcDir $(WNS) --noedgeToEdge
-$(EXEC_PATH)/tuneGen --srcDir $(WNS) --noedgeToEdge
+${EXEC_PATH}/tuneGen --srcDir ${WNS} ${SPATIAL}
+${EXEC_PATH}/tuneGen --srcDir ${WNS} ${SPATIAL}
+${EXEC_PATH}/tuneGen --srcDir ${WNS} ${SPATIAL}
 
-$(EXEC_PATH)/tuneGen --srcDir $(WNS) --noedgeToEdge
-$(EXEC_PATH)/tuneGen --srcDir $(WNS) --noedgeToEdge
-$(EXEC_PATH)/tuneGen --srcDir $(WNS) --noedgeToEdge
+${EXEC_PATH}/tuneGen --srcDir ${WNS} ${SPATIAL}
+${EXEC_PATH}/tuneGen --srcDir ${WNS} ${SPATIAL}
+${EXEC_PATH}/tuneGen --srcDir ${WNS} ${SPATIAL}
 
-$(EXEC_PATH)/tuneGen --srcDir $(WNS) --noedgeToEdge
-$(EXEC_PATH)/tuneGen --srcDir $(WNS) --noedgeToEdge
-$(EXEC_PATH)/tuneGen --srcDir $(WNS) --noedgeToEdge
+${EXEC_PATH}/tuneGen --srcDir ${WNS} ${SPATIAL}
+${EXEC_PATH}/tuneGen --srcDir ${WNS} ${SPATIAL}
+${EXEC_PATH}/tuneGen --srcDir ${WNS} ${SPATIAL}
 
 ## network spread
-$(EXEC_PATH)/tuneGen --srcDir $(WNS) --edgeToEdge
-$(EXEC_PATH)/tuneGen --srcDir $(WNS) --edgeToEdge
-$(EXEC_PATH)/tuneGen --srcDir $(WNS) --edgeToEdge
+${EXEC_PATH}/tuneGen --srcDir ${WNS} ${EDGE_TO_EDGE}
+${EXEC_PATH}/tuneGen --srcDir ${WNS} ${EDGE_TO_EDGE}
+${EXEC_PATH}/tuneGen --srcDir ${WNS} ${EDGE_TO_EDGE}
 
-$(EXEC_PATH)/tuneGen --srcDir $(WNS) --edgeToEdge
-$(EXEC_PATH)/tuneGen --srcDir $(WNS) --edgeToEdge
-$(EXEC_PATH)/tuneGen --srcDir $(WNS) --edgeToEdge
+${EXEC_PATH}/tuneGen --srcDir ${WNS} ${EDGE_TO_EDGE}
+${EXEC_PATH}/tuneGen --srcDir ${WNS} ${EDGE_TO_EDGE}
+${EXEC_PATH}/tuneGen --srcDir ${WNS} ${EDGE_TO_EDGE}
 
-$(EXEC_PATH)/tuneGen --srcDir $(WNS) --noedgeToEdge
-$(EXEC_PATH)/tuneGen --srcDir $(WNS) --noedgeToEdge
-$(EXEC_PATH)/tuneGen --srcDir $(WNS) --noedgeToEdge
+${EXEC_PATH}/tuneGen --srcDir ${WNS} ${SPATIAL}
+${EXEC_PATH}/tuneGen --srcDir ${WNS} ${SPATIAL}
+${EXEC_PATH}/tuneGen --srcDir ${WNS} ${SPATIAL}
 
 
 
 ########################################
-## run sims (correctly specified)
+## run sims {correctly specified}
 
 ## wns
-$(EXEC_PATH)/runM1Mles --srcDir $(WNS) --noedgeToEdge
+${EXEC_PATH}/runM1Mles --srcDir ${WNS} ${SPATIAL}
 
 ## spatial spread
-$(EXEC_PATH)/runM1Mles --srcDir $(WNS) --noedgeToEdge
-$(EXEC_PATH)/runM1Mles --srcDir $(WNS) --noedgeToEdge
-$(EXEC_PATH)/runM1Mles --srcDir $(WNS) --noedgeToEdge
+${EXEC_PATH}/runM1Mles --srcDir ${WNS} ${SPATIAL}
+${EXEC_PATH}/runM1Mles --srcDir ${WNS} ${SPATIAL}
+${EXEC_PATH}/runM1Mles --srcDir ${WNS} ${SPATIAL}
 
-$(EXEC_PATH)/runM1Mles --srcDir $(WNS) --noedgeToEdge
-$(EXEC_PATH)/runM1Mles --srcDir $(WNS) --noedgeToEdge
-$(EXEC_PATH)/runM1Mles --srcDir $(WNS) --noedgeToEdge
+${EXEC_PATH}/runM1Mles --srcDir ${WNS} ${SPATIAL}
+${EXEC_PATH}/runM1Mles --srcDir ${WNS} ${SPATIAL}
+${EXEC_PATH}/runM1Mles --srcDir ${WNS} ${SPATIAL}
 
-$(EXEC_PATH)/runM1Mles --srcDir $(WNS) --noedgeToEdge
-$(EXEC_PATH)/runM1Mles --srcDir $(WNS) --noedgeToEdge
-$(EXEC_PATH)/runM1Mles --srcDir $(WNS) --noedgeToEdge
+${EXEC_PATH}/runM1Mles --srcDir ${WNS} ${SPATIAL}
+${EXEC_PATH}/runM1Mles --srcDir ${WNS} ${SPATIAL}
+${EXEC_PATH}/runM1Mles --srcDir ${WNS} ${SPATIAL}
 
 ## network spread
-$(EXEC_PATH)/runM1Mles --srcDir $(WNS) --edgeToEdge
-$(EXEC_PATH)/runM1Mles --srcDir $(WNS) --edgeToEdge
-$(EXEC_PATH)/runM1Mles --srcDir $(WNS) --edgeToEdge
+${EXEC_PATH}/runM1Mles --srcDir ${WNS} ${EDGE_TO_EDGE}
+${EXEC_PATH}/runM1Mles --srcDir ${WNS} ${EDGE_TO_EDGE}
+${EXEC_PATH}/runM1Mles --srcDir ${WNS} ${EDGE_TO_EDGE}
 
-$(EXEC_PATH)/runM1Mles --srcDir $(WNS) --edgeToEdge
-$(EXEC_PATH)/runM1Mles --srcDir $(WNS) --edgeToEdge
-$(EXEC_PATH)/runM1Mles --srcDir $(WNS) --edgeToEdge
+${EXEC_PATH}/runM1Mles --srcDir ${WNS} ${EDGE_TO_EDGE}
+${EXEC_PATH}/runM1Mles --srcDir ${WNS} ${EDGE_TO_EDGE}
+${EXEC_PATH}/runM1Mles --srcDir ${WNS} ${EDGE_TO_EDGE}
 
-$(EXEC_PATH)/runM1Mles --srcDir $(WNS) --noedgeToEdge
-$(EXEC_PATH)/runM1Mles --srcDir $(WNS) --noedgeToEdge
-$(EXEC_PATH)/runM1Mles --srcDir $(WNS) --noedgeToEdge
+${EXEC_PATH}/runM1Mles --srcDir ${WNS} ${SPATIAL}
+${EXEC_PATH}/runM1Mles --srcDir ${WNS} ${SPATIAL}
+${EXEC_PATH}/runM1Mles --srcDir ${WNS} ${SPATIAL}
 
 
 ########################################
-## run sims (incorrectly specified)
+## run sims {incorrectly specified}
 
 ## wns
-$(EXEC_PATH)/runM1MlesMiss --srcDir $(WNS) --noedgeToEdge
+${EXEC_PATH}/runM1MlesMiss --srcDir ${WNS} ${SPATIAL}
 
 ## spatial spread
-$(EXEC_PATH)/runM1MlesMiss --srcDir $(WNS) --noedgeToEdge
-$(EXEC_PATH)/runM1MlesMiss --srcDir $(WNS) --noedgeToEdge
-$(EXEC_PATH)/runM1MlesMiss --srcDir $(WNS) --noedgeToEdge
+${EXEC_PATH}/runM1MlesMiss --srcDir ${WNS} ${SPATIAL}
+${EXEC_PATH}/runM1MlesMiss --srcDir ${WNS} ${SPATIAL}
+${EXEC_PATH}/runM1MlesMiss --srcDir ${WNS} ${SPATIAL}
 
-$(EXEC_PATH)/runM1MlesMiss --srcDir $(WNS) --noedgeToEdge
-$(EXEC_PATH)/runM1MlesMiss --srcDir $(WNS) --noedgeToEdge
-$(EXEC_PATH)/runM1MlesMiss --srcDir $(WNS) --noedgeToEdge
+${EXEC_PATH}/runM1MlesMiss --srcDir ${WNS} ${SPATIAL}
+${EXEC_PATH}/runM1MlesMiss --srcDir ${WNS} ${SPATIAL}
+${EXEC_PATH}/runM1MlesMiss --srcDir ${WNS} ${SPATIAL}
 
-$(EXEC_PATH)/runM1MlesMiss --srcDir $(WNS) --noedgeToEdge
-$(EXEC_PATH)/runM1MlesMiss --srcDir $(WNS) --noedgeToEdge
-$(EXEC_PATH)/runM1MlesMiss --srcDir $(WNS) --noedgeToEdge
+${EXEC_PATH}/runM1MlesMiss --srcDir ${WNS} ${SPATIAL}
+${EXEC_PATH}/runM1MlesMiss --srcDir ${WNS} ${SPATIAL}
+${EXEC_PATH}/runM1MlesMiss --srcDir ${WNS} ${SPATIAL}
 
 ## network spread
-$(EXEC_PATH)/runM1MlesMiss --srcDir $(WNS) --edgeToEdge
-$(EXEC_PATH)/runM1MlesMiss --srcDir $(WNS) --edgeToEdge
-$(EXEC_PATH)/runM1MlesMiss --srcDir $(WNS) --edgeToEdge
+${EXEC_PATH}/runM1MlesMiss --srcDir ${WNS} ${EDGE_TO_EDGE}
+${EXEC_PATH}/runM1MlesMiss --srcDir ${WNS} ${EDGE_TO_EDGE}
+${EXEC_PATH}/runM1MlesMiss --srcDir ${WNS} ${EDGE_TO_EDGE}
 
-$(EXEC_PATH)/runM1MlesMiss --srcDir $(WNS) --edgeToEdge
-$(EXEC_PATH)/runM1MlesMiss --srcDir $(WNS) --edgeToEdge
-$(EXEC_PATH)/runM1MlesMiss --srcDir $(WNS) --edgeToEdge
+${EXEC_PATH}/runM1MlesMiss --srcDir ${WNS} ${EDGE_TO_EDGE}
+${EXEC_PATH}/runM1MlesMiss --srcDir ${WNS} ${EDGE_TO_EDGE}
+${EXEC_PATH}/runM1MlesMiss --srcDir ${WNS} ${EDGE_TO_EDGE}
 
-$(EXEC_PATH)/runM1MlesMiss --srcDir $(WNS) --noedgeToEdge
-$(EXEC_PATH)/runM1MlesMiss --srcDir $(WNS) --noedgeToEdge
-$(EXEC_PATH)/runM1MlesMiss --srcDir $(WNS) --noedgeToEdge
+${EXEC_PATH}/runM1MlesMiss --srcDir ${WNS} ${SPATIAL}
+${EXEC_PATH}/runM1MlesMiss --srcDir ${WNS} ${SPATIAL}
+${EXEC_PATH}/runM1MlesMiss --srcDir ${WNS} ${SPATIAL}
