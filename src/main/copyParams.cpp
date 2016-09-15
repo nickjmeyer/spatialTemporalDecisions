bf1d5d1f2191b87a42ebc3f0c0e19854c44aaea3
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-09-15 11:44:32 -0400

    script to copy parameters

--- src/main/copyParams.cpp
+++ src/main/copyParams.cpp
@@ -1,12 +1,33 @@
+#include <gflags/gflags.h>
+#include <glog/logging.h>
 #include "system.hpp"
 #include "model.hpp"
 #include "model2GravityEDist.hpp"
 #include "model2EdgeToEdge.hpp"
 
+DEFINE_string(srcDir,"","Path to source directory");
+DEFINE_bool(edgeToEdge,false,"Edge to edge transmission");
+DEFINE_string(outDir,"","Path to save parameters");
+
+
 template <typename T>
-copyParams<T>(const boost::filesystem path) {
+void copyParams(const boost::filesystem::path path) {
+	System<T,T> s;
+	s.modelGen_r.read();
+	s.modelGen_r.save_to(path);
 }
 
 
-int main() {
+int main(int argc, char ** argv) {
+  ::google::InitGoogleLogging(argv[0]);
+  ::google::ParseCommandLineFlags(&argc,&argv,true);
+  njm::sett.setup(std::string(argv[0]),FLAGS_srcDir);
+
+	const boost::filesystem::path path(FLAGS_outDir);
+
+	if(FLAGS_edgeToEdge) {
+		copyParams<Model2EdgeToEdge>(path);
+	} else {
+    copyParams<Model2GravityEDist>(path);
+	}
 }
