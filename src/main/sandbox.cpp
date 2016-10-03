1dfc73c3728160afb8c5b34bcbd06aaf19f122bc
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-10-03 16:20:13 -0400

    clean up sandbox

--- src/main/sandbox.cpp
+++ src/main/sandbox.cpp
@@ -9,6 +9,8 @@ int main(int argc, char ** argv){
   ::gflags::ParseCommandLineFlags(&argc,&argv,true);
   njm::sett.setup(std::string(argv[0]),FLAGS_srcDir);
 
+  const bool edgeToEdge = false;
+
   typedef Model2GravityEDist MG;
 
   typedef MG ME;
@@ -21,24 +23,17 @@ int main(int argc, char ** argv){
   typedef VanillaRunnerNS<S,RA> RR;
 
   S s("obsData.txt");
-  s.setEdgeToEdge(FLAGS_edgeToEdge);
+  s.setEdgeToEdge(edgeToEdge);
   s.modelEst_r = s.modelGen_r;
   s.revert();
 
   int numReps = 500;
   Starts starts("startingLocations.txt");
 
-  NT nt;
-  MA ma;
-  PA pa;
-  RP rp;
 
-  RN rn;
   RA ra;
-  RM rm;
   RR rr;
-  ra.setEdgeToEdge(FLAGS_edgeToEdge);
-  ma.setEdgeToEdge(FLAGS_edgeToEdge);
+  ra.setEdgeToEdge(edgeToEdge);
   // ra.reset();
 
   double valRA = rr.run(s,ra,numReps,s.fD.finalT,starts).smean();
