5d221d7a2e5ab250b42e2c9be6fdab6b6c6226b9
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-10-14 17:47:47 -0400

    remove test chunk

--- src/main/rankAgent.cpp
+++ src/main/rankAgent.cpp
@@ -93,9 +93,6 @@ void RankAgent<F,M>::applyTrt(const SimData & sD,
             infRanks = f.infFeat * (tp.weights + jitter);
             notRanks = f.notFeat * (tp.weights + jitter);
         } else {
-            for (j = 0; j < f.numFeatures; ++j) {
-                njm::rnorm01();
-            }
             // calculate ranks
             infRanks = f.infFeat * tp.weights;
             notRanks = f.notFeat * tp.weights;
