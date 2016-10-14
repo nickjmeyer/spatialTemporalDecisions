b390c0e93e14874d7ef91a78beaee3289de97e81
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-10-14 16:31:18 -0400

    adjust

--- src/main/model.cpp
+++ src/main/model.cpp
@@ -611,7 +611,7 @@ void ModelBase::estimateMle(const std::vector<double> & startingVals,
 
     int iter = 0;
     int status;
-    const int maxIter = 200;
+    const int maxIter = 500;
     do{
         iter++;
         status = gsl_multimin_fdfminimizer_iterate(s);
