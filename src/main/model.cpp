1d33834cc48e22e900002057e47b9a23480f73a8
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-10-14 14:17:29 -0400

    adjust convergence criteria

--- src/main/model.cpp
+++ src/main/model.cpp
@@ -618,7 +618,7 @@ void ModelBase::estimateMle(const std::vector<double> & startingVals,
         if(status)
             break;
 
-        status = gsl_multimin_test_gradient(s->gradient,1e-3);
+        status = gsl_multimin_test_gradient(s->gradient,0.1);
 
     }while(status == GSL_CONTINUE && iter < 100);
 
