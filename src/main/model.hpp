1beba84677da655b3e7e2c66c567998bd2b8e642
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-10-14 21:33:04 -0400

    start adding a logll val and grad function

--- src/main/model.hpp
+++ src/main/model.hpp
@@ -13,175 +13,180 @@
 enum Estimation {INVALID = -1, MLE = 0, MLES = 1, MCMC = 2};
 
 class ModelBase {
- // protected:
- public:
-  unsigned int numPars;
-  std::string name;
-  int set;
-  std::vector<ParamBase *> pars;
-  std::vector<double> probs;
-  std::vector<double> expitInfProbs;
-  std::vector<double> expitRevProbs;
-  std::vector<double> quick;
+    // protected:
+public:
+    unsigned int numPars;
+    std::string name;
+    int set;
+    std::vector<ParamBase *> pars;
+    std::vector<double> probs;
+    std::vector<double> expitInfProbs;
+    std::vector<double> expitRevProbs;
+    std::vector<double> quick;
 
-  arma::colvec meanHit;
-  arma::mat varHit;
-  int ready;
-  int numInfected,numNotInfec;
-  int fixSample;
+    arma::colvec meanHit;
+    arma::mat varHit;
+    int ready;
+    int numInfected,numNotInfec;
+    int fixSample;
 
-  // defaults to false
-  bool edgeToEdge;
+    // defaults to false
+    bool edgeToEdge;
 
- public:
-  ModelBase(){ };
-  ModelBase(const std::string & str,
-	    const std::vector<ParamBase *> & newPars,
-	    const FixedData & fD);
-  ModelBase(const std::string & str,
-    const std::vector<ParamBase *> & newPars);
-  virtual ~ModelBase();
+public:
+    ModelBase(){ };
+    ModelBase(const std::string & str,
+            const std::vector<ParamBase *> & newPars,
+            const FixedData & fD);
+    ModelBase(const std::string & str,
+            const std::vector<ParamBase *> & newPars);
+    virtual ~ModelBase();
 
-  virtual void init(const FixedData & fD);
+    virtual void init(const FixedData & fD);
 
-  virtual void read();
-  virtual void read_from(const boost::filesystem::path path);
+    virtual void read();
+    virtual void read_from(const boost::filesystem::path path);
 
-  virtual void save() const;
-  virtual void save_to(const boost::filesystem::path path) const;
+    virtual void save() const;
+    virtual void save_to(const boost::filesystem::path path) const;
 
-  virtual void infProbs(const SimData & sD,
-			const TrtData & tD,
-			const FixedData & fD,
-			const DynamicData & dD);
+    virtual void infProbs(const SimData & sD,
+            const TrtData & tD,
+            const FixedData & fD,
+            const DynamicData & dD);
 
-  virtual std::vector<double> infProbs();
+    virtual std::vector<double> infProbs();
 
-  virtual void revProbs(const SimData & sD,
-			const TrtData & tD,
-			const FixedData & fD,
-			const DynamicData & dD);
+    virtual void revProbs(const SimData & sD,
+            const TrtData & tD,
+            const FixedData & fD,
+            const DynamicData & dD);
 
-  virtual std::vector<double> revProbs();
+    virtual std::vector<double> revProbs();
 
-  virtual void setFill(const SimData & sD,
-		       const TrtData & tD,
-		       const FixedData & fD,
-		       const DynamicData & dD);
+    virtual void setFill(const SimData & sD,
+            const TrtData & tD,
+            const FixedData & fD,
+            const DynamicData & dD);
 
-  virtual void modFill(const SimData & sD,
-		       const TrtData & tD,
-		       const FixedData & fD,
-		       const DynamicData & dD);
+    virtual void modFill(const SimData & sD,
+            const TrtData & tD,
+            const FixedData & fD,
+            const DynamicData & dD);
 
-  virtual void setQuick(const SimData & sD,
-			const TrtData & tD,
-			const FixedData & fD,
-			const DynamicData & dD);
+    virtual void setQuick(const SimData & sD,
+            const TrtData & tD,
+            const FixedData & fD,
+            const DynamicData & dD);
 
-  virtual std::vector<double> & getQuick();
+    virtual std::vector<double> & getQuick();
 
-  virtual double oneOnOne(const int notNode, const int infNode,
-			  const int numNodes) const;
+    virtual double oneOnOne(const int notNode, const int infNode,
+            const int numNodes) const;
 
-  virtual std::vector<double> partial(const int notNode,
-				      const int infNode,
-				      const SimData & sD,
-				      const TrtData & tD,
-				      const FixedData & fD,
-				      const DynamicData & dD);
+    virtual std::vector<double> partial(const int notNode,
+            const int infNode,
+            const SimData & sD,
+            const TrtData & tD,
+            const FixedData & fD,
+            const DynamicData & dD);
 
-  virtual std::vector<double> partial2(const int notNode,
-				       const int infNode,
-				       const SimData & sD,
-				       const TrtData & tD,
-				       const FixedData & fD,
-				       const DynamicData & dD);
+    virtual std::vector<double> partial2(const int notNode,
+            const int infNode,
+            const SimData & sD,
+            const TrtData & tD,
+            const FixedData & fD,
+            const DynamicData & dD);
 
-  virtual void setFisher(const SimData & sD,
-			 const TrtData & tD,
-			 const FixedData & fD,
-			 const DynamicData & dD);
+    virtual void setFisher(const SimData & sD,
+            const TrtData & tD,
+            const FixedData & fD,
+            const DynamicData & dD);
 
-  virtual bool sample(const bool force = false);
+    virtual bool sample(const bool force = false);
 
-  virtual void revert();
+    virtual void revert();
 
-  virtual void fit(const SimData & sD, const TrtData & tD,
-		   const FixedData & fD, const DynamicData & dD);
+    virtual void fit(const SimData & sD, const TrtData & tD,
+            const FixedData & fD, const DynamicData & dD);
 
-  virtual void fit(const SimData & sD, const TrtData & tD,
-		   const FixedData & fD, const DynamicData & dD,
-		   const bool init);
+    virtual void fit(const SimData & sD, const TrtData & tD,
+            const FixedData & fD, const DynamicData & dD,
+            const bool init);
 
 
-  virtual void fit(const std::vector<double> & startingVals,
-		   const SimData & sD, const TrtData & tD,
-		   const FixedData & fD, const DynamicData & dD);
+    virtual void fit(const std::vector<double> & startingVals,
+            const SimData & sD, const TrtData & tD,
+            const FixedData & fD, const DynamicData & dD);
 
-  virtual std::vector<double> getPar() const;
+    virtual std::vector<double> getPar() const;
 
-  virtual std::vector<double>
-  getPar(const std::vector<std::string> & name) const;
+    virtual std::vector<double>
+    getPar(const std::vector<std::string> & name) const;
 
-  virtual std::vector<double>::const_iterator
-  putPar(std::vector<double>::const_iterator it);
+    virtual std::vector<double>::const_iterator
+    putPar(std::vector<double>::const_iterator it);
 
-  virtual void setPar(const std::string & name, const double & val);
-  virtual void setPar(const std::vector<std::string> & name,
-		      const double & val);
+    virtual void setPar(const std::string & name, const double & val);
+    virtual void setPar(const std::vector<std::string> & name,
+            const double & val);
 
-  virtual void linScale(const double & scale);
+    virtual void linScale(const double & scale);
 
-  virtual void setType(const Estimation & est);
-  virtual void setFixSample(const int & fix);
-  virtual int getFixSample();
-  virtual Estimation getType() const;
-  virtual Estimation & getType();
+    virtual void setType(const Estimation & est);
+    virtual void setFixSample(const int & fix);
+    virtual int getFixSample();
+    virtual Estimation getType() const;
+    virtual Estimation & getType();
 
-  Estimation fitType;
+    Estimation fitType;
 
-  virtual void setEdgeToEdge(const bool edgeToEdge);
-  virtual bool getEdgeToEdge() const;
+    virtual void setEdgeToEdge(const bool edgeToEdge);
+    virtual bool getEdgeToEdge() const;
 
-  virtual void estimateMle(const std::vector<double> & startingVals,
-			   const SimData & sD,
-			   const TrtData & tD,
-			   const FixedData & fD,
-			   const DynamicData & dD);
+    virtual void estimateMle(const std::vector<double> & startingVals,
+            const SimData & sD,
+            const TrtData & tD,
+            const FixedData & fD,
+            const DynamicData & dD);
 
-  virtual double logll(const SimData & sD,
-		       const TrtData & tD,
-		       const FixedData & fD,
-		       const DynamicData & dD);
+    virtual double logll(const SimData & sD,
+            const TrtData & tD,
+            const FixedData & fD,
+            const DynamicData & dD);
 
-  virtual std::vector<double> logllGrad(const SimData & sD,
-					const TrtData & tD,
-					const FixedData & fD,
-					const DynamicData & dD);
+    virtual std::vector<double> logllGrad(const SimData & sD,
+            const TrtData & tD,
+            const FixedData & fD,
+            const DynamicData & dD);
 
-  virtual std::vector<double> logllHess(const SimData & sD,
-					const TrtData & tD,
-					const FixedData & fD,
-                                        const DynamicData & dD);
+    virtual std::vector<double> logllGradBoth(const SimData & sD,
+            const TrtData & tD,
+            const FixedData & fD,
+            const DynamicData & dD);
+
+    virtual std::vector<double> logllHess(const SimData & sD,
+            const TrtData & tD,
+            const FixedData & fD,
+            const DynamicData & dD);
 
 };
 
 
 class ModelBaseFitObj {
- public:
+public:
 
-  ModelBaseFitObj(ModelBase * const mb,
-		  const SimData sD,
-		  const TrtData tD,
-		  const FixedData fD,
-		  const DynamicData dD);
+    ModelBaseFitObj(ModelBase * const mb,
+            const SimData sD,
+            const TrtData tD,
+            const FixedData fD,
+            const DynamicData dD);
 
-  ModelBase * mb;
-  SimData sD;
-  TrtData tD;
-  FixedData fD;
-  DynamicData dD;
+    ModelBase * mb;
+    SimData sD;
+    TrtData tD;
+    FixedData fD;
+    DynamicData dD;
 };
 
 
