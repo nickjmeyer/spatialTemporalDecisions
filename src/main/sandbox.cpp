3f89c688d4a9f27549f5cfbaee3431e10de829b7
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-10-17 22:08:06 -0400

    try 100

--- src/main/sandbox.cpp
+++ src/main/sandbox.cpp
@@ -36,7 +36,7 @@ int main(int argc, char ** argv){
             s.modelEst_r = s.modelGen_r;
             s.revert();
 
-            int numReps = 250;
+            int numReps = 100;
             Starts starts("startingLocations.txt");
 
             NT nt;
@@ -95,7 +95,7 @@ int main(int argc, char ** argv){
             s.modelEst_r = s.modelGen_r;
             s.revert();
 
-            int numReps = 250;
+            int numReps = 100;
             Starts starts("startingLocations.txt");
 
             NT nt;
