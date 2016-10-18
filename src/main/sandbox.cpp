e47a86abaa0b1bb5efff59ee9c7bcd7a35c61cce
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-10-17 22:15:30 -0400

    try 7

--- src/main/sandbox.cpp
+++ src/main/sandbox.cpp
@@ -95,7 +95,7 @@ int main(int argc, char ** argv){
             s.modelEst_r = s.modelGen_r;
             s.revert();
 
-            int numReps = 8;
+            int numReps = 7;
             Starts starts("startingLocations.txt");
 
             NT nt;
