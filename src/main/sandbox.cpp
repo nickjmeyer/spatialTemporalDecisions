444dc4554997edf89c1888ab545cd47117c5c994
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-10-23 21:07:29 -0400

    setup sandbox to debug

--- src/main/sandbox.cpp
+++ src/main/sandbox.cpp
@@ -6,47 +6,71 @@ using namespace google;
 using namespace gflags;
 
 
+DEFINE_string(srcDir,"","Path to source directory");
+DEFINE_bool(edgeToEdge,false,"Edge to edge transmission");
+DEFINE_bool(dryRun,false,"Do not execute main");
+
 int main(int argc, char ** argv){
     InitGoogleLogging(argv[0]);
     ParseCommandLineFlags(&argc,&argv,true);
+    if(!FLAGS_dryRun) {
+        njm::sett.setup(std::string(argv[0]),FLAGS_srcDir);
 
-    std::vector<int> fips;
-    njm::fromFile(fips,"data/wns/fips.txt");
-    const int numNodes = fips.size();
-    CHECK_EQ(1128,numNodes);
+        njm::toFile(FLAGS_edgeToEdge,
+                njm::sett.datExt("edgeToEdge_flag_",".txt"));
 
-    std::vector< std::vector<int> > obsData;
-    {
-        std::vector<int> obsData_raw;
-        njm::fromFile(obsData_raw,"data/wns/obsData.txt");
+        if(FLAGS_edgeToEdge) {
+            LOG(FATAL) << "Supposed to debug spatial spread.";
+        } else {
+            // typedef ModelTimeExpCavesGPowGDistTrendPowCon MG;
+            typedef Model2GravityEDist MG;
 
-        std::vector<int> add;
-        std::vector<int>::const_iterator it,end;
-        it = obsData_raw.begin();
-        end = obsData_raw.end();
-        int count = 0;
-        while(it != end) {
-            add.push_back(*it);
-            ++it;
-            ++count;
-            if (count == numNodes) {
-                obsData.push_back(add);
-                add.clear();
-                count = 0;
+            typedef MG ME;
+
+            typedef System<MG,ME> S;
+
+            typedef MyopicAgent<ME> MA;
+
+            typedef FitOnlyRunner<S,MA> R_MA;
+
+
+            S s;
+            s.setEdgeToEdge(FLAGS_edgeToEdge);
+            s.modelGen_r.setType(MLES);
+            s.modelEst_r.setType(MLES);
+
+            int numReps = 100;
+            Starts starts(numReps,s.fD.numNodes);
+
+            MA ma;
+
+            ma.setEdgeToEdge(FLAGS_edgeToEdge);
+
+            R_MA r_ma;
+
+            int r,t;
+
+            r = 65;
+            njm::resetSeed(r);
+            s.reset(starts[r]);
+
+            for(t=s.sD.time; t<s.fD.finalT; t++){
+                if(t>=s.fD.trtStart && s.sD.numNotInfec > 0){
+                    s.modelEst.fit(s.sD,s.tD,s.fD,s.dD,
+                            t > s.fD.trtStart);
+
+                    ma.applyTrt(s.sD,s.tD,s.fD,s.dD,
+                            s.modelEst);
+                }
+
+                s.updateStatus();
+
+                s.nextPoint();
+
             }
         }
-        CHECK_EQ(0,count);
-    }
 
-    for (int i = 0; i < (obsData.size() - 1); ++i) {
-        for (int j = 0; j < numNodes; ++j) {
-            const int before = obsData.at(i).at(j)/2;
-            const int after = obsData.at(i+1).at(j)/2;
-            CHECK_LE(before,after)
-                << "node " << j << " failed for time point "
-                << i << " to " << i+1;
-        }
+        njm::sett.clean();
     }
-
     return 0;
 }
