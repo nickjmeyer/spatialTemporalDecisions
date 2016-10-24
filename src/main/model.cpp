67a2a8e32d5b9a30930c7f950de3cdea3dabf8bc
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-10-24 14:50:28 -0400

    fix gradient check

--- src/main/model.cpp
+++ src/main/model.cpp
@@ -652,7 +652,7 @@ int ModelBase::estimateMle(const std::vector<double> & startingVals,
         gradVals.push_back(gsl_vector_get(s->gradient,pi));
     }
 
-    int gradCheck = gsl_multimin_test_gradient(s->gradient,0.1);
+    int gradCheck = gsl_multimin_test_gradient(s->gradient,1e-6);
 
     int errorCode = 0;
     if (raiseError && status != GSL_SUCCESS && status != GSL_CONTINUE
