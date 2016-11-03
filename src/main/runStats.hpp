c7ee9462c843359867822ece4000cf616c6e6891
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-11-03 12:35:30 -0400

    add reservoir capability to run stats

--- src/main/runStats.hpp
+++ src/main/runStats.hpp
@@ -10,26 +10,33 @@ protected:
     std::vector<double> vals;
     unsigned int n;
 
+    std::vector<double> reservoir;
+    unsigned int reservoir_cnt;
+
     double sMean_; // sample mean
     double sSqMean_; // sample mean of squared values
     double sVar_; // sample variance
     double sSd_; // sample standard deviation
     double seMean_; // standard error of mean
 
-    void update(const double & add);
-    void update(const std::vector<double> & add);
-
 public:
     RunStats();
     RunStats(const std::vector<double> & init);
+    RunStats(const unsigned int & num_vals);
+    RunStats(const unsigned int & num_vals,
+            const std::vector<double> & init);
 
-    void operator () (const std::vector<double> & add){
-        update(add);
-    };
+    void update(const double & add);
 
-    void operator () (const double & add){
-        update(add);
-    };
+    void update(const unsigned int & position,
+            const double & add);
+
+    void update(const std::vector<double> & add);
+
+    void update(const std::vector<unsigned int> & positions,
+            const std::vector<double> & add);
+
+    void squash();
 
     double sMean() const;
 
