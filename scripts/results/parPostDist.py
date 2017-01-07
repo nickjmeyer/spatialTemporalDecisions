236b599b844fcd42bdaa5904a8c56df9d2ed61d2
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-01-07 14:39:21 -0500

    add new lines

--- scripts/results/parPostDist.py
+++ scripts/results/parPostDist.py
@@ -80,8 +80,9 @@ def main():
     for name, (postMean, postL95, postU95), simVal in zip(edgeParNames,
                                                           edgePostDist,
                                                           edgeSimVals):
-        print "%s & %0.2f & [ %0.2f, %0.2f] & %0.2f" % (name, postMean, postL95,
-                                                        postU95, simVal)
+        print "%s & %0.2f & [ %0.2f, %0.2f] & %0.2f \\\\" % (name, postMean,
+                                                             postL95, postU95,
+                                                             simVal)
 
 
     ## get spatial values
@@ -99,8 +100,9 @@ def main():
     for name, (postMean, postL95, postU95), simVal in zip(spatialParNames,
                                                           spatialPostDist,
                                                           spatialSimVals):
-        print "%s & %0.2f & [ %0.2f, %0.2f] & %0.2f" % (name, postMean, postL95,
-                                                        postU95, simVal)
+        print "%s & %0.2f & [ %0.2f, %0.2f] & %0.2f \\\\" % (name, postMean,
+                                                             postL95, postU95,
+                                                             simVal)
 
 
 if __name__ == "__main__":
