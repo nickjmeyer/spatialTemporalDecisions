8bfb5942001785332b84af190b5b72de64c362bb
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-01-07 14:32:26 -0500

    add xlables

--- scripts/plotting/covariatePostDist.R
+++ scripts/plotting/covariatePostDist.R
@@ -10,17 +10,21 @@ edgePar = read.table(paste(dirName, "/sampStats_edgeToEdge2_param_edge_.txt", se
 
 
 pdf("../../data/plotting/covariate_post_dist_spatial_uninfected.pdf")
-boxplot(spatialPar[,2:5], ylim = range(spatialPar[,2:9]))
+boxplot(spatialPar[,2:5], ylim = range(spatialPar[,2:9]),
+        names = c("Caves", "Cold days", "Area", "SR"))
 dev.off()
 
 pdf("../../data/plotting/covariate_post_dist_spatial_infected.pdf")
-boxplot(spatialPar[,6:9], ylim = range(spatialPar[,2:9]))
+boxplot(spatialPar[,6:9], ylim = range(spatialPar[,2:9]),
+        names = c("Caves", "Cold days", "Area", "SR"))
 dev.off()
 
 pdf("../../data/plotting/covariate_post_dist_edge_uninfected.pdf")
-boxplot(edgePar[,2:5], ylim = range(edgePar[,2:9]))
+boxplot(edgePar[,2:5], ylim = range(edgePar[,2:9]),
+        names = c("Caves", "Cold days", "Area", "SR"))
 dev.off()
 
 pdf("../../data/plotting/covariate_post_dist_edge_infected.pdf")
-boxplot(edgePar[,6:9], ylim = range(edgePar[,2:9]))
+boxplot(edgePar[,6:9], ylim = range(edgePar[,2:9]),
+        names = c("Caves", "Cold days", "Area", "SR"))
 dev.off()
