688677ac0fa7c3b334dbd8d05dbc8454fc9b752f
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-02-06 21:14:27 -0500

    add histogram script for ranking weights

---
+++ scripts/plotting/weightHist.R
@@ -0,0 +1,24 @@
+rm(list=ls(all=TRUE))
+
+dirName = "../../data/wns/2017-01-26-10-33-30/"
+
+allFiles = dir(dirName)
+
+weightsFiles = c()
+for(f in allFiles) {
+  if (grepl("^rank_M1Sp_weights_[0-9]{1,2}_.txt$",f)) {
+    weightsFiles = c(weightsFiles,f)
+  }
+}
+
+weights = c()
+for(f in weightsFiles) {
+  w = read.table(paste(dirName, f, sep = ""), header = FALSE)
+  stopifnot(nrow(w) == 8)
+  weights = rbind(weights, w[nrow(w),])
+}
+weights = as.matrix(weights)
+
+pdf("../../data/plotting/weights.pdf")
+boxplot(weights, names = c(expression(eta[1]),expression(eta[2]),expression(eta[3])))
+dev.off()
