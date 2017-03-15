84dc21b4047a4692e33767b976b07b76152ea479
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-03-15 11:35:53 -0400

    add scripts to run crp 1000 for 25 steps

---
+++ scripts/runCrp1000For25.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## quit on error
+set -e
+
+PROJ_ROOT=.
+
+EXEC_PATH=${PROJ_ROOT}/build/src/main
+
+TOY=${PROJ_ROOT}/data/toy
+
+DRY=--nodryRun
+
+SPATIAL="--noedgeToEdge ${DRY}"
+
+########################################
+## run sims {correctly specified}
+
+## toy spatial spread
+${EXEC_PATH}/copyParams --srcDir ${WNS} --outDir ${TOY}/crp1000 ${SPATIAL}
+
+${EXEC_PATH}/tuneGen --srcDir ${TOY}/crp1000 ${SPATIAL}
+
+${EXEC_PATH}/runM1Mles --srcDir ${TOY}/crp1000 ${SPATIAL}
